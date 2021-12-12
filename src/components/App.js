ff91bc95ee1dc262e77e09638a7d10643e9fc7ed
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-12 16:55:19 +0100

    Added routes to main component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./LandingPage"
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+  );
+}
+
+export default App;
