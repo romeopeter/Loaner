a44aff4979877caad290bc6226cf914ac9f946df
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-12 16:59:15 +0100

    landing page component

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import OrderbookLayout from "./OrderbookLayout";
+
+export default function LandingPage() {
+	return (
+		<OrderbookLayout>
+			<header className="App-header">
+				<h4>Orderbook is still under construction</h4>
+			</header>
+		</OrderbookLayout>
+	)
+}
