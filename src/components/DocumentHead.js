9da51bbc68a110277df08c151476b78e832669d5
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-15 07:00:03 +0100

    Document head with React Helmet package

--- src/components/DocumentHead.js
+++ src/components/DocumentHead.js
@@ -30,6 +30,10 @@ function DocumentHead(props) {
 					rel: "stylesheet",
 					href: "https://fonts.googleapis.com/css2?family=Arvo&family=Lato:wght@400&display=swap",
 				},
+				{
+					rel: "stylesheet",
+					href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+				}
 			].concat(links)}
 		/>
 	);
