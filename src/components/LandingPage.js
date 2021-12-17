632a5db6f583cb61d2c8212497d5d2d52ad512a2
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-17 03:00:02 +0100

    Modified component

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -17,102 +17,129 @@ export default function LandingPage() {
 			<DocumentHead title="Orderbook | Home" />
 			<OrderbookLayout>
 				<header id="orderbook-header" style={setBgImage(headerBanner)}>
-					<div id="orderbook-intro">
-						<h1 className="mt-0">
-							Orderbook, your go to financial platform.
-						</h1>
-						<p className="font-md text-white">
-							Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit, sed do eiusmod tempor incididunt ut labore et
-							dolore magna aliqua. Ut enim ad minim veniam, quis
-							nostrud exercitation.
-						</p>
-						<Button
-							title="Learn more"
-							link="#"
-							buttonClass="intro-cta"
-						/>
+					<div className="grid grid-cols-1 gap-x-6">
+						<div id="orderbook-intro">
+							<h1 className="mt-0">
+								Orderbook, your go to financial platform.
+							</h1>
+							<p className="font-md">
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua. Ut
+								enim ad minim veniam, quis nostrud exercitation.
+							</p>
+							<Button
+								title="Learn more"
+								link="#"
+								buttonClass="intro-cta"
+							/>
+						</div>
 					</div>
 				</header>
 				<section id="orderbook-rows">
-					<div className="container border-right">
-						<div className="gray-marker">
-							
-						</div>
-						<div className="row-details">
-							<h3>orderbook your go</h3>
-							<p>
-								Excepteur sint occaecat cupidatat non proident,
-								sunt in culpa qui.
-							</p>
-						</div>
-					</div>
-					<div className="container border-right">
-						<div className="gray-marker">
-							
-						</div>
-						<div className="row-details">
-							<h3>orderbook your go</h3>
-							<p>
-								Excepteur sint occaecat cupidatat non proident,
-								sunt in culpa qui.
-							</p>
-						</div>
-					</div>
-					<div className="container">
-						<div className="gray-marker">
-							
-						</div>
-						<div className="row-details">
-							<h3>orderbook your go</h3>
-							<p>
-								Excepteur sint occaecat cupidatat non proident,
-								sunt in culpa qui.
-							</p>
+					<div class="mx-auto sm:px-6 lg:max-w-7xl lg:px-8">
+						<div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+							<div className="container right-border border-r border-black">
+								<div className="row-details">
+									<h3>
+										<i
+											className="fa fa-thumbs-o-up"
+											aria-hidden="true"
+										></i>
+										orderbook your go
+									</h3>
+									<p>
+										Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui.
+									</p>
+								</div>
+							</div>
+							<div className="container right-border border-r border-black">
+								<div className="row-details">
+									<h3>
+										<i
+											className="fa fa-thumbs-o-up"
+											aria-hidden="true"
+										></i>
+										orderbook your go
+									</h3>
+									<p>
+										Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui.
+									</p>
+								</div>
+							</div>
+							<div className="container">
+								<div className="row-details">
+									<h3>
+										<i
+											className="fa fa-thumbs-o-up"
+											aria-hidden="true"
+										></i>
+										orderbook your go
+									</h3>
+									<p>
+										Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui.
+									</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				</section>
 				<section id="orderbook-bids">
-					<div className="container" id="bid-intro">
-						<h2>As an investor your money grows for you</h2>
-						<p>
-							Ullamco laboris nisi ut aliquip ex ea commodo
-							consequat. Duis aute irure dolor in reprehenderit in
-							voluptate velit esse cillum dolore eu fugiat nulla
-							pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia.
-						</p>
-						<Button
-							title="View live bids"
-							link="#"
-							buttonClass="bid-cta"
+					<div class="mt-6 grid sm:grid-cols-1 md:grid-cols-2">
+						<div className="bid-container" id="bid-intro">
+							<h2>As an investor your money grows for you</h2>
+							<p>
+								Ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in
+								reprehenderit in voluptate velit esse cillum
+								dolore eu fugiat nulla pariatur. Excepteur sint
+								occaecat cupidatat non proident, sunt in culpa
+								qui officia.
+							</p>
+							<Button
+								title="View live bids"
+								link="#"
+								buttonClass="bid-cta"
+							/>
+						</div>
+						{/*Renders image*/}
+						<img
+							className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+							id="bid-banner"
+							alt="Lady with a tablet"
+							src={tabletLady}
 						/>
 					</div>
-					{/*Renders image*/}
-					<img
-						className="container"
-						id="bid-banner"
-						alt="Lady with a tablet"
-						src={tabletLady}
-					/>
 				</section>
 				<section id="orderbook-request">
 					<div id="request-intro-container">
-						<img src={requestBanner} alt="" id="request-banner" />
-						<div id="request-intro">
-							<h2>Orderbook, your go to financial platform</h2>
-							<p>
-								Dui faucibus in ornare quam viverra orci
-								sagittis. Habitasse platea dictumst vestibulum
-								rhoncus est pellentesque elit ullamcorper. In
-								nulla posuere sollicitudin aliquam ultrices
-								sagittis orci a.
-							</p>
-							<Button
-								title="Create a request"
-								link="#"
-								buttonClass="request-cta"
+						<div className="grid sm:grid-cols-1 md:grid-cols-2">
+							<img
+								className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+								src={requestBanner}
+								alt=""
+								id="request-banner"
 							/>
+							<div id="request-intro">
+								<h2>
+									Orderbook, your go to financial platform
+								</h2>
+								<p>
+									Dui faucibus in ornare quam viverra orci
+									sagittis. Habitasse platea dictumst
+									vestibulum rhoncus est pellentesque elit
+									ullamcorper. In nulla posuere sollicitudin
+									aliquam ultrices sagittis orci a.
+								</p>
+								<Button
+									title="Create a request"
+									link="#"
+									buttonClass="request-cta"
+								/>
+							</div>
 						</div>
 					</div>
 					<div id="orderbook-steps">
@@ -120,36 +147,40 @@ export default function LandingPage() {
 							<div className="white-marker">1</div>
 							<h4>Create a free account</h4>
 							<p>
-								Et molestie ac feugiat sed lectus vestibulum. 
-								Cursus turpis massa tincidunt dui ut. 
-								Euismod in pellentesque massa placerat duis ultricies lacus.
+								Et molestie ac feugiat sed lectus vestibulum.
+								Cursus turpis massa tincidunt dui ut. Euismod in
+								pellentesque massa placerat duis ultricies
+								lacus.
 							</p>
 						</div>
 						<div className="request-step">
 							<div className="white-marker">2</div>
 							<h4>Start a new request</h4>
 							<p>
-								Et molestie ac feugiat sed lectus vestibulum. 
-								Cursus turpis massa tincidunt dui ut. 
-								Euismod in pellentesque massa placerat duis ultricies lacus.
+								Et molestie ac feugiat sed lectus vestibulum.
+								Cursus turpis massa tincidunt dui ut. Euismod in
+								pellentesque massa placerat duis ultricies
+								lacus.
 							</p>
 						</div>
 						<div className="request-step">
 							<div className="white-marker">3</div>
 							<h4>Accept investors list</h4>
 							<p>
-								Et molestie ac feugiat sed lectus vestibulum. 
-								Cursus turpis massa tincidunt dui ut. 
-								Euismod in pellentesque massa placerat duis ultricies lacus.
+								Et molestie ac feugiat sed lectus vestibulum.
+								Cursus turpis massa tincidunt dui ut. Euismod in
+								pellentesque massa placerat duis ultricies
+								lacus.
 							</p>
 						</div>
 						<div className="request-step">
 							<div className="white-marker">4</div>
 							<h4>Recieve your fund</h4>
 							<p>
-								Et molestie ac feugiat sed lectus vestibulum. 
-								Cursus turpis massa tincidunt dui ut. 
-								Euismod in pellentesque massa placerat duis ultricies lacus.
+								Et molestie ac feugiat sed lectus vestibulum.
+								Cursus turpis massa tincidunt dui ut. Euismod in
+								pellentesque massa placerat duis ultricies
+								lacus.
 							</p>
 						</div>
 					</div>
