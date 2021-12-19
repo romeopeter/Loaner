0c21b27f8e5c83a6327b11ff10c20094b2709366
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-19 01:42:20 +0100

    Buttom componet

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Button(props) {
-	const {title, link, buttonClass} = props;
+	const { type, title, link, buttonClass } = props;
 
 	return (
-		<button id="default-orderbook-button" className={buttonClass}>
+		<button
+			type={type ? type : ""}
+			id="default-orderbook-button"
+			className={buttonClass}
+		>
 			<Link to={link}>{title}</Link>
 		</button>
 	);
