49fce034d2d3ef352ad9f2b68b0943ddf612a982
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-19 01:40:59 +0100

    Added login route

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage"
+import Login from "./authentication/Login"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
