83bac8a5a0eed4b342242bdff2f36f472ed56ee3
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-19 22:26:36 +0100

    Added register component to route

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage"
 import Login from "./authentication/Login"
+import Register from "./authentication/Register"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
