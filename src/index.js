18b6717d798c1ac40fe27cc73c77760a0b9bbe61
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-22 19:49:04 +0100

    Added redux provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter} from "react-router-dom";
+import { Provider } from 'react-redux'
 import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
 import "./assets/styles/index.css";
 import "./assets/styles/main.scss";
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById("root")
@@ -18,4 +21,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
