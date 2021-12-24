c1689613ef706838284d9448b117dd58768ce1fe
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-24 02:40:57 +0100

    Redux authentication slice

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,10 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
-
 /**
  * Auth slice reads/ writes to the auth store (store.js). Also exports reducer and actions.
- * 
+ *
  *
  * **/
+import { createSlice } from "@reduxjs/toolkit";
+import { signUpFunc, signInFunc, signOutFunc } from "./creators";
 
 const initialState = {
 	email: "",
@@ -14,17 +14,27 @@ const initialState = {
 	lastName: "",
 	password: "",
 	confirmPassword: "",
-	// role: "",
+	role: "",
 };
 
 export const authSlice = createSlice({
 	name: "auth",
 	initialState: initialState,
 	reducers: {
-    	signUp: (state) => console.log("sign up"),
-    	signOut: (state) => console.log("sign out")
-  	},
+		signUp: (state, action) => {
+			const signUpResponse = signUpFunc(action.payload);
+			state.auth = signUpResponse;
+		},
+		signIn: (state, action) => {
+			const signInResponse = signInFunc(action.payload);
+			state.auth = signInResponse;
+		},
+		signOut: (state, action) => {
+			const signOutResponse = signOutFunc();
+			state.auth = signOutResponse;
+		},
+	},
 });
 
-export const {signUp, signOut} = authSlice.actions;
-export default authSlice.reducer
+export const { signUp, signIn, signOut } = authSlice.actions;
+export default authSlice.reducer;
