a15006a7d2bae4ef5515fff4a345bf809972de88
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-24 16:41:50 +0100

    Modified sign-in modification

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useLocation } from "react-redux";
 
-import OrderbookLayout from "../OrderbookLayout";
+import useAuth from "./useAuth";
+import { signInAction } from "../../redux/authSlice";
+
 import DocumentHead from "../DocumentHead";
 import Button from "../Button";
+
 import setBgImage from "../../utils/setBgImage";
 import phoneLady from "../../assets/images/phoneLady.jpg";
 
@@ -11,18 +15,51 @@ export default function Login() {
 	const pageName = "Login";
 
 	const [form, setForm] = useState({
-		email: "",
+		emailAddress: "",
 		password: "",
 		isChecked: false,
 	});
 
+	const { signIn } = useAuth();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const {state} = useLocation();
+
+	const handleChange = (e) => {
+		const target = e.target;
+		const name = target.name;
+		const value =
+			target.type === "checkbox" ? target.checked : target.value;
+
+		setForm((state) => {
+			return {
+				...state,
+				[name]: value,
+			};
+		});
+
+		console.log(form);
+	};
+
+	// Input from form state
+	const { emailAddress, password } = form;
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// Callback is Redux hook dispatching sign-in action (Login requst)
+		const cb = dispatch(signInAction({ emailAddress, password }));
+
+		// signIn is useAuth hook's method for signing user in and redirecting to dashboard
+		signIn(cb).then(() => {
+			navigate(state?.path || "/dashboard");
+		});
+	};
+
 	return (
 		<>
 			<DocumentHead title={pageName} />
-			<section
-				id="orderbook-login"
-				className="orderbook-authentication"
-			>
+			<section id="orderbook-login" className="orderbook-authentication">
 				<div className="grid grid-cols-1 md:grid-cols-2 h-full">
 					<div
 						id="intro-background"
@@ -45,19 +82,26 @@ export default function Login() {
 							<p>
 								Lorem ipsum dolor sit amet, consectetur
 								adipiscing elit, sed do eiusmod tempor
-								incididunt ut labore et dolore magna aliqua.
-								Ut enim ad minim veniam, quis nostrud
-								exercitation.
+								incididunt ut labore et dolore magna aliqua. Ut
+								enim ad minim veniam, quis nostrud exercitation.
 							</p>
 						</div>
 						<div id="overlay"></div>
 					</div>
 
 					<div id="orderbook-form">
-						<form className="h-full">
+						<form
+							className="h-full"
+							onSubmit={handleSubmit}
+						>
 							<div className="pt-20 pb-10 px-12">
-								<h1 id="orderbook-home" className="text-center mb-10 leading-6 md:hidden">
-									<Link to="/" className="text-gray-400">Orderbook Online</Link>
+								<h1
+									id="orderbook-home"
+									className="text-center mb-10 leading-6 md:hidden"
+								>
+									<Link to="/" className="text-gray-400">
+										Orderbook Online
+									</Link>
 								</h1>
 								<div className="px-4 sm:px-0 mb-3">
 									<h2 className="text-lg font-medium leading-6 pb-3 sm:pb-2">
@@ -77,6 +121,7 @@ export default function Login() {
 											placeholder="Email"
 											className="mt-1 focus:ring-white block w-full sm:text-sm bg-gray-300 form-field"
 											required
+											onChange={(e) => handleChange(e)}
 										/>
 									</div>
 
@@ -89,6 +134,7 @@ export default function Login() {
 											placeholder="Password"
 											className="mt-1 focus:ring-white block w-full sm:text-sm bg-gray-300 form-field"
 											required
+											onChange={(e) => handleChange(e)}
 										/>
 									</div>
 
@@ -101,6 +147,9 @@ export default function Login() {
 													id="persist-login"
 													className="focus:ring-white h-4 w-4 text-indigo-600 border-black rounded"
 													required
+													onChange={(e) =>
+														handleChange(e)
+													}
 												/>
 											</div>
 											<div className="ml-3 text-sm">
@@ -126,8 +175,8 @@ export default function Login() {
 
 									<div className="col-span-6 sm:col-span-4 mt-1">
 										<Button
-											title="Login"
 											type="submit"
+											title="Login"
 											buttonClass="login-button auth-button"
 										/>
 									</div>
@@ -136,9 +185,7 @@ export default function Login() {
 										id="dont-have-account"
 										className="col-span-6 sm:col-span-4 mt-1 text-center account-signal"
 									>
-										<span>
-											Don't have an account yet?
-										</span>{" "}
+										<span>Don't have an account yet?</span>{" "}
 										<Link to="/register">Sign up</Link>
 									</div>
 								</div>
