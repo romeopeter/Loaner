af349c8df75e316c93a4e68ea5652a74d8356937
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-24 16:21:57 +0100

    imported private routes component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./LandingPage"
-import Login from "./authentication/Login"
-import Register from "./authentication/Register"
+import LandingPage from "./LandingPage";
+import Login from "./authentication/Login";
+import Register from "./authentication/Register";
+import Dashboard from "./dashboard/Dashboard";
+import RequireAuth from "./authentication/RequireAuth";
 
 function App() {
   return (
@@ -9,6 +11,14 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route
+        path="/dashboard"
+        element={
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        }
+      />
     </Routes>
   );
 }
