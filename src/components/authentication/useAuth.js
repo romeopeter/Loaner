dd90f81643e57d14264c539f12d204574897be5d
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-25 23:28:21 +0100

    Modified

--- src/components/authentication/useAuth.js
+++ src/components/authentication/useAuth.js
@@ -1,29 +1,21 @@
 /**
  * Provdes a scheme to verify/determine user authenticated status.
- * **/ 
+ * **/
 
-import {useState} from 'react'
+import { useState } from "react";
 
 export default function useAuth() {
 	const [authed, setAuthed] = useState(false);
 
 	return {
 		authed,
-		signOut(cb) {
-			if (cb) {
-				return new Promise(res => {
-					setAuthed(false)
-					res()	
-				})
-			};
+		signOut() {
+			setAuthed(false);
+			return authed;
 		},
-		signIn(cb) {
-			if (cb) {
-				return new Promise(res => {
-					setAuthed(true)
-					res()	
-				})
-			};
+		signIn() {
+			setAuthed(true);
+			return authed;
 		},
-	}
+	};
 }
