5ed30635612c6c8fab88af898a5cb9a007782345
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-27 14:05:17 +0100

    Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from "./authSlice"
+import messageReducer from "./messageSlice"
 
 export default configureStore({
   reducer: {
     auth: authReducer,
+    message: messageReducer
   }
 })
