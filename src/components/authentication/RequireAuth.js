fd1d9d64046636858091e481d80f2b32018132ce
Romeo-Peter <romeomeshack34@gmail.com>
2021-12-27 14:01:02 +0100

    Auth pages component

--- src/components/authentication/RequireAuth.js
+++ src/components/authentication/RequireAuth.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import useAuth from "./useAuth";
 
 export default function RequireAuth({ children }) {
-	const { authed } = useAuth();
 	const location = useLocation();
 
-	return authed === true ? (
+	const isLoggedIn = JSON.parse(localStorage.getItem("IS_LOGGED_IN"));
+
+	return isLoggedIn === true ? (
 		children
 	) : (
 		<Navigate to="/login" replace state={{ path: location.pathname }} />
