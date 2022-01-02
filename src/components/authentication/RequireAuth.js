0590b26a4c90269a569b128fa3cd678b18667aff
Romeo-Peter <romeomeshack34@gmail.com>
2022-01-02 22:38:13 +0100

    Modified

--- src/components/authentication/RequireAuth.js
+++ src/components/authentication/RequireAuth.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function RequireAuth({ children }) {
 	const location = useLocation();
 
-	const currentUser = JSON.parse(localStorage.getItem("USER"));
-	const isLoggedIn = currentUser.tokens.access;
+	const { isLoggedIn } = useSelector((state) => state.auth);
 	const testLogin = true;
 
 	return isLoggedIn === true ? (
