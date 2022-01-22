5266b616e3a2e34de69f5d9296f621cafc8fa6b4
Romeo-Peter <romeomeshack34@gmail.com>
2022-01-22 12:10:32 +0100

    Modified loan request loanRequest service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -5,10 +5,10 @@ const API_URL = "https://order-book-online.herokuapp.com";
 // Default Axios config
 const axiosHeaders = axios.defaults.headers;
 const currentUser = JSON.parse(localStorage.getItem("USER"));
-axiosHeaders.post['Content-Type'] = "application/json";
+axiosHeaders.post["Content-Type"] = "application/json";
 
 if (currentUser?.tokens.access !== undefined) {
-	axiosHeaders.common['Authorization'] = `Token ${currentUser.tokens.access}`
+	axiosHeaders.common["Authorization"] = `Bearer ${currentUser.tokens.access}`;
 }
 
 /*Authentication requests*/
@@ -23,12 +23,11 @@ const signInRequest = (data) => {
 		.post(`${API_URL}/api/v1/registration/sign_in/`, data)
 		.then((response) => {
 			if (response.data.tokens.access) {
-
 				// Store access token in browser storage
-				localStorage.setItem("USER", JSON.stringify(response.data))
+				localStorage.setItem("USER", JSON.stringify(response.data));
 			}
 
-			return response.data
+			return response.data;
 		})
 		.catch((error) => error);
 };
@@ -38,15 +37,12 @@ const signOutRequest = () => {
 };
 
 /*Authentication requests*/
-const submitLoanRequest = (data) => {
-	const requestURL = "https://order-book-online.herokuapp.com/";
+const loanRequest = (data) => {
+	const requestURL = "https://order-book-online.herokuapp.com";
 
-	return axios.post(`${requestURL}/api/v1/loan_request/cp/`, data).then(response => {
-		if(response.status === 200 || response.status === 201) {
-			return response
-		}
-	}).catch(error => error)
-}
+	return axios
+		.post(`${requestURL}/api/v1/loan_request/cp/`, data)
+		.catch((error) => error);
+};
 
-
-export {signUpRequest, signInRequest, submitLoanRequest};
+export { signUpRequest, signInRequest, loanRequest };
