a1289d231cc32f311099ae3d57453edc6526b02d
Romeo-Peter <romeomeshack34@gmail.com>
2022-01-27 16:52:40 +0100

    Removed loan request service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,12 +4,7 @@ const API_URL = "https://order-book-online.herokuapp.com";
 
 // Default Axios config
 const axiosHeaders = axios.defaults.headers;
-const currentUser = JSON.parse(localStorage.getItem("USER"));
 axiosHeaders.post["Content-Type"] = "application/json";
-
-if (currentUser?.tokens.access !== undefined) {
-	axiosHeaders.common["Authorization"] = `Bearer ${currentUser.tokens.access}`;
-}
 
 /*Authentication requests*/
 const signUpRequest = (data) => {
@@ -37,22 +32,4 @@ const signOutRequest = () => {
 	localStorage.removeItem("USER");
 };
 
-/*Authentication requests*/
-const loanRequest = (data) => {
-	const requestURL = "https://order-book-online.herokuapp.com";
-
-	return axios
-		.post(`${requestURL}/api/v1/loan_request/cp/`, data)
-		.catch((error) => error);
-
-        // Get refresh token
-        const {tokens: {refresh}} = JSON.parse(localStorage.getItem("USER"));
-
-        // Modify header in config
-
-        // Make new request for access token
-
-        // Save token in storage
-};
-
-export { signUpRequest, signInRequest, loanRequest };
+export { signUpRequest, signInRequest};
