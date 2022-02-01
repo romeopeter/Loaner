ff8b6e088671ef93ddaab25a06304ae9f4c8e857
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-01 14:58:29 +0100

    Added style prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Button(props) {
-	const { type, title, link, buttonClass, buttonDisabled, handleClick, children } =
-		props;
+	const { 
+		type, 
+		title, 
+		link, 
+		buttonClass, 
+		buttonDisabled, 
+		handleClick, 
+		style, 
+		children 
+	} = props;
 
 	if (link) {
 		return (
 			<button
 				type={type ? type : "button"}
 				className={`default-orderbook-button ${buttonClass}`}
+				style={style}
 				disabled={buttonDisabled}
 			>
 				<Link to={link}>{title}</Link>
@@ -20,6 +29,7 @@ export default function Button(props) {
 			<button
 				type={type ? type : "button"}
 				className={`default-orderbook-button ${buttonClass}`}
+				style={style}
 				disabled={buttonDisabled}
 				onClick={handleClick && (() => handleClick())}
 			>
