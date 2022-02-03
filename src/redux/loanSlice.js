dd08176fe77aa6a7b0f2e94e05f178e31e755337
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-03 05:32:14 +0100

    Added loan slice and modified auth slice

--- src/redux/loanSlice.js
+++ src/redux/loanSlice.js
@@ -6,7 +6,7 @@
 
 import { createSlice } from "@reduxjs/toolkit";
 
-import { loanRequest } from "../services/loan.service.js";
+import { loanRequestCP, loanRequestBond } from "../services/loan.service.js";
 
 import { setServerMessage } from "./messageSlice";
 
@@ -22,8 +22,24 @@ export const loanSlice = createSlice({
 
 export default loanSlice.reducer;
 
-export const asyncLoanRequest = (data) => (dispatch) => {
-    return loanRequest(data).then((response) => {
+export const asyncCPLoanRequest = (data) => (dispatch) => {
+    return loanRequestCP(data).then((response) => {
+
+        if(response.status === 200 || response.status === 201) {
+           dispatch(setServerMessage("Loan created succesfully"));
+        }
+
+        // Check error
+        if ("stack" in response) {
+            dispatch(setServerMessage(response.message));
+        }
+
+        return response
+    });
+};
+
+export const asyncBondLoanRequest = (data) => (dispatch) => {
+    return loanRequestBond(data).then((response) => {
 
         if(response.status === 200 || response.status === 201) {
            dispatch(setServerMessage("Loan created succesfully"));
