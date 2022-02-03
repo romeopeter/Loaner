1be362504aa61715b15c65117339978cd58ed0c3
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-03 05:32:14 +0100

    Added loan slice and modified auth slice

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -4,82 +4,83 @@
  *
  * **/
 
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { setServerMessage } from "./messageSlice";
 import {
 	signUpRequest,
 	signInRequest,
 	signOutRequest,
 } from "../services/auth.service.js";
 
-import { setServerMessage } from "./messageSlice";
-
 const user = JSON.parse(localStorage.getItem("USER"));
 
 const authState = user
-	? { isLoggedIn: true, user }
-	: { isLoggedIn: false, user: null };
+	? { isLoggedIn: true, user, error: null }
+	: { isLoggedIn: false, user: null, error: null };
+
+export const signInAction = createAsyncThunk("auth/signInAction", async credentials => {
+	const res = await signInRequest(credentials);
+
+	// Check if error object exist
+	if ("stack" in res) {
+		return res
+	}
+
+	return res.data;
+});
+
 
 export const authSlice = createSlice({
 	name: "auth",
 	initialState: authState,
 	reducers: {
-		signUpAction: (state, action) => {
-			return;
-		},
-		signInAction: (state, action) => {
-			const payload = action.payload;
-
-			state = {
-				...state,
-				isLoggedIn: true,
-				user: payload.user
-			};
-		},
 		signOutAction: (state, action) => {
-			return localStorage.removeItem("USER");
+			state.isLoggedIn = false;
+			state.user = null;
+
+			localStorage.removeItem("USER");
 		},
 	},
+	extraReducers: {
+		[signInAction.pending]: (state, action) => {
+			console.log("Loading");
+		},
+		[signInAction.rejected]: (state, action) => {
+			console.log(action.payload);
+			console.log("rejected");
+		},
+		[signInAction.fulfilled]: (state, action) => {
+			const payload = action.payload;
+
+			// Store user object in browser storage
+			localStorage.setItem("USER", JSON.stringify(payload));
+
+			state.isLoggedIn = true;
+			state.user = payload;
+		},
+	}
 });
 
-export const { signUpAction, signInAction, signOutAction } = authSlice.actions;
+export const { signUpAction, signOutAction } = authSlice.actions;
 export default authSlice.reducer;
 
 export const signUpAsync = (data) => (dispatch) => {
 	return signUpRequest(data).then((response) => {
-
 		// Check if error stack object exist
 		if ("stack" in response) {
-			const {data, status} = response.response;
+			const { data, status } = response.response;
 			const message = data.error;
 
 			dispatch(setServerMessage(message));
 
-			return response
+			return response;
 		}
 
 		if (response.status === 201) {
 			dispatch(setServerMessage("Please sign in."));
 
-			return response
+			return response;
 		}
-	});
-};
-
-export const signInAsync = (data) => (dispatch) => {
-	return signInRequest(data).then((response) => {
-		
-		// Check error
-		if ("stack" in response) {
-			const message = response.message;
-			// console.log(response.response)
-
-			dispatch(setServerMessage(message));
-		}
-
-		dispatch(signInAction(response));
-
-		return response
 	});
 };
 
