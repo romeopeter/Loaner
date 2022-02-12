c073adaa42c79509b3629f7b0d3c5dd992ee3a85
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-12 16:35:45 +0100

    Removed unnnecessary hook

--- src/components/dashboard/broker/CreateOffer.js
+++ src/components/dashboard/broker/CreateOffer.js
@@ -1,6 +1,6 @@
 import React, { useState, createRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import OrderbookLayout from "../../OrderbookLayout";
 import OfferForm from "./OfferForm";
