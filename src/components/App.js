9f535e1ba809626e25c15e18645c1d6eed27f8d3
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-14 22:34:58 +0100

    App

--- src/components/App.js
+++ src/components/App.js
@@ -14,38 +14,7 @@ import ArchivedDeals from "./dashboard/ArchivedDeals";
 import SingleTrancheDeal from "./dashboard/SingleTrancheDeal";
 import MultipleTrancheDeal from "./dashboard/MultipleTrancheDeal";
 
-import ClientDashboard from "./dashboard/client/ClientDashboard";
-import LoanRequest from "./dashboard/client/LoanRequest";
-import Offers from "./dashboard/client/Offers";
-import ShowOffer from "./dashboard/client/ShowOffer";
-import EditOffer from "./dashboard/client/EditOffer";
-import PublishOffer from "./dashboard/client/PublishOffer";
-import ViewInvestor from "./dashboard/client/ViewInvestor";
-
-import InvestorDashboard from "./dashboard/investor/InvestorDashboard";
-import InvestorDashboard2 from "./dashboard/investor/InvestorDashboard2";
-import InvestorOffers from "./dashboard/investor/InvestorOffers";
-import InvestorBids from "./dashboard/investor/InvestorBids";
-import InvestorDeclinedBids from "./dashboard/investor/InvestorDeclinedBids";
-
-import BrokerDashboard from "./dashboard/broker/Dashboard";
-import NewClient from "./dashboard/broker/NewClient";
-import AllClients from "./dashboard/broker/AllClients";
-import CreateOffer from "./dashboard/broker/CreateOffer"; 
-import NewOfferTiming from "./dashboard/broker/NewOfferTiming";
-import LoanSummary from "./dashboard/broker/LoanSummary";
-import AllLoans from "./dashboard/broker/AllLoans";
-import LoanOffer from "./dashboard/broker/LoanOffer";
-import SelectInvestor from "./dashboard/broker/SelectInvestor";
-import NewClientSave from "./dashboard/broker/NewClientSave";
-
-import AdminCurrencies from "./dashboard/admin/AdminCurrencies";
-import AdminCompanies from "./dashboard/admin/AdminCompanies";
-import AdminTranche from "./dashboard/admin/AdminTranche";
-import AdminPreference from "./dashboard/admin/AdminPreferenc";
-import AdminProfile from "./dashboard/admin/AdminProfileSettings";
-import PrivacyPolicy from "./dashboard/admin/PrivacyPolicy";
-import Terms from "./dashboard/admin/Terms";
+import { Client, Investor, Broker, Admin } from "./ComponentsRoute";
 
 function App() {
   return (
@@ -55,14 +24,6 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        <Route path="/current-deals" element={<CurrentDeals />} />
-        <Route path="/archived-deals" element={<ArchivedDeals />} />
-        <Route path="single-tranche-deal" element={<SingleTrancheDeal />} />
-        <Route
-          path="/multiple-tranche-deal"
-          element={<MultipleTrancheDeal />}
-        />
 
         {/*Profile*/}
         <Route
@@ -74,247 +35,25 @@ function App() {
           }
         />
 
+        <Route path="/current-deals" element={<CurrentDeals />} />
+        <Route path="/archived-deals" element={<ArchivedDeals />} />
+        <Route path="single-tranche-deal" element={<SingleTrancheDeal />} />
+        <Route
+          path="/multiple-tranche-deal"
+          element={<MultipleTrancheDeal />}
+        />
+
         {/*Client dashboard*/}
-        <Route
-          path="/client/dashboard"
-          element={
-            <RequireAuth>
-              <ClientDashboard />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/client/new-loan"
-          element={
-            <RequireAuth>
-              <LoanRequest />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/client/offers"
-          element={
-            <RequireAuth>
-              <Offers />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/client/offers/offer"
-          element={
-            <RequireAuth>
-              <ShowOffer />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/client/offers/offer/edit"
-          element={
-            <RequireAuth>
-              <EditOffer />
-            </RequireAuth>
-          }
-        />
-
-        <Route
-          path="/client/offers/offer/publish"
-          element={
-            <RequireAuth>
-              <PublishOffer />
-            </RequireAuth>
-          }
-        />
-
-        <Route
-          path="/client/offers/offer/view-investors"
-          element={
-            <RequireAuth>
-              <ViewInvestor />
-            </RequireAuth>
-          }
-        />
+        <Client />
 
         {/*Investor Dashboard*/}
-        <Route
-          path="/investor/dashboard"
-          element={
-            <RequireAuth>
-              <InvestorDashboard />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/investor/offers"
-          element={
-            <RequireAuth>
-              <InvestorDashboard2 />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/investor/offers/offer"
-          element={
-            <RequireAuth>
-              <InvestorOffers />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/investor/bids"
-          element={
-            <RequireAuth>
-              <InvestorBids />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/investor/bids/declined"
-          element={
-            <RequireAuth>
-              <InvestorDeclinedBids />
-            </RequireAuth>
-          }
-        />
+        <Investor />
 
         {/*Broker dashboard*/}
-        <Route
-          path="/broker/dashboard"
-          element={
-            <RequireAuth>
-            <BrokerDashboard />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/broker/dashboard/new-client"
-          element={
-            <RequireAuth>
-            <NewClient />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/broker/dashboard/new-client-save"
-          element={
-            <RequireAuth>
-            <NewClientSave />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/broker/dashboard/allclients"
-          element={
-            <RequireAuth>
-            <AllClients />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/broker/dashboard/create-offer"
-          element={
-            <RequireAuth>
-            <CreateOffer />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/broker/dashboard/new-offer-timing"
-          element={
-            <RequireAuth>
-            <NewOfferTiming />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/broker/dashboard/new-offer/summary"
-          element={
-            <RequireAuth>
-            <LoanSummary />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/broker/dashboard/allloans/"
-          element={
-            <RequireAuth>
-            <AllLoans/>
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/broker/dashboard/loan-offer/"
-          element={
-            <RequireAuth>
-            <LoanOffer />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/broker/dashboard/loan-offer/select-investor"
-          element={
-            <RequireAuth>
-            <SelectInvestor />
-            </RequireAuth>
-          }
-        />
+        <Broker />
 
         {/*Admin Dashboard*/}
-        <Route
-          path="/admin/currencies"
-          element={
-            <RequireAuth>
-            <AdminCurrencies />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/admin/companies"
-          element={
-            <RequireAuth>
-            <AdminCompanies />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/admin/tranche"
-          element={
-            <RequireAuth>
-            <AdminTranche />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/admin/preference"
-          element={
-            <RequireAuth>
-            <AdminPreference />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/admin/profile-settings"
-          element={
-            <RequireAuth>
-            <AdminProfile />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/admin/privacy"
-          element={
-            <RequireAuth>
-            <PrivacyPolicy />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/admin/terms"
-          element={
-            <RequireAuth>
-            <Terms />
-            </RequireAuth>
-          }
-        />
+        <Admin />
       </Routes>
     </ChakraProvider>
   );
