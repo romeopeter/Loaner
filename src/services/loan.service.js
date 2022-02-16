e7b2bcbcdffdbf543c9c327151aaa6ce77ad511a
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-16 23:04:06 +0100

    Loan service

--- src/services/loan.service.js
+++ src/services/loan.service.js
@@ -1,33 +1,19 @@
 import axios from "axios";
 import axiosConfig from "./authHeader";
 
-axiosConfig();
-
-const API_URL = "https://order-book-online.herokuapp.com";
-
-const currentUser = JSON.parse(localStorage.getItem("USER"));
-const accessToken = currentUser.tokens.access;
-
-const header = {headers: {
-	"Authorization": `${accessToken}`,
-}}
-
 const getOffers = () => {
-	return axios.get(`${API_URL}/api/v1/loan_request/`, header).catch(error => console.log(error));
-}
-
+	return axios
+		.get("/api/v1/loan_request/")
+		.catch((error) => console.log(error));
+};
 
 const loanRequestCP = (data) => {
-
-	return axios
-		.post(`${API_URL}/api/v1/loan_request/cp/`, data, header)
-		.catch((error) => error);
+	return axios.post("/api/v1/loan_request/cp/", data).catch((error) => error);
 };
 
 const loanRequestBond = (data) => {
-
 	return axios
-		.post(`${API_URL}/api/v1/loan_request/bonds/`, data, header)
+		.post("/api/v1/loan_request/bonds/", data)
 		.catch((error) => error);
 };
 
