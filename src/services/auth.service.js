2fc8cf1cd08a122860afb15bbfd8712255ce19c6
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-16 23:04:50 +0100

    Authentication service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,20 +1,16 @@
 import axios from "axios";
 import axiosConfig from "./authHeader";
 
-axiosConfig();
-
-const API_URL = "https://order-book-online.herokuapp.com";
-
 /*Authentication requests*/
 const signUpRequest = (data) => {
 	return axios
-		.post(`${API_URL}/api/v1/registration/sign_up/?is_superuser=True`, data)
+		.post("api/v1/registration/sign_up/?is_superuser=True", data)
 		.catch((error) => error);
 };
 
 const signInRequest = (data) => {
 	return axios
-		.post(`${API_URL}/api/v1/registration/sign_in/`, data)
+		.post("api/v1/registration/sign_in/", data)
 		.catch((error) => error);
 };
 
