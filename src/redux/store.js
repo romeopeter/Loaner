57b47f87c6a9cb7abbe97ae4e2307e50974092f5
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-18 04:37:14 +0100

    Added investors categories and lists reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from "./authSlice"
 import messageReducer from "./messageSlice"
 import loanReducer from "./loanSlice"
+import investorListReducer from "./investorListSlice"
+import investorsCategoriesReducer from "./investorCategorySlice"
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     message: messageReducer,
-    loan: loanReducer
+    loan: loanReducer,
+    investorsCategories: investorsCategoriesReducer,
+    investorsList: investorListReducer
   }
 })
