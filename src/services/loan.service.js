bafaa40ed13819c7e9caa5a021fb592d9e663549
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-18 04:42:37 +0100

    Added http request for investors list and category

--- src/services/loan.service.js
+++ src/services/loan.service.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import axiosConfig from "./authHeader";
+
+// Get loan request
 
 const getOffers = () => {
 	return axios
@@ -17,4 +18,41 @@ const loanRequestBond = (data) => {
 		.catch((error) => error);
 };
 
+// Investor list endpoints
+const saveInvestorsList = (data) => {
+	return axios.post("/v1/lists/investor", data).catch((error) => error);
+};
+
+const getInvestorsList = () => {
+	return axios
+		.get("/v1/lists/investor/")
+		.catch((error) => console.log(error));
+};
+
+const getInvestor = (id) => {
+	return axios
+		.get(`/v1/list/investor/${id}/`)
+		.catch((error) => console.log(error));
+};
+
+// Investors category
+const getInvestorsCategories = () => {
+	return axios
+		.get("/v1/lists/investor_category/")
+		.catch((error) => console.log(error));
+};
+const createInvestorsCategories = () => {
+	return axios
+		.post("/v1/lists/investor_category/")
+		.catch((error) => console.log(error));
+};
+
+const getInvestorsCategory = (id) => {
+	return axios
+		.get(`/v1/lists/investor_category/${id}/`)
+		.catch((error) => console.log(error));
+};
+
 export { loanRequestCP, loanRequestBond, getOffers };
+export { saveInvestorsList, getInvestorsList, getInvestor };
+export { getInvestorsCategories, createInvestorsCategories, getInvestorsCategory };
