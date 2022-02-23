298f53fa4eae6ea0a997321d337e837a8b23d1f6
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-23 19:06:31 +0100

    Alloffers

--- src/components/dashboard/investor/AllOffers.js
+++ src/components/dashboard/investor/AllOffers.js
@@ -1,8 +1,5 @@
-import React, { createRef, useState } from "react";
-
+import React, {createRef} from "react";
 import { Link, useNavigate, Navigate } from "react-router-dom";
-
-import ReactPaginate from "react-paginate";
 
 import OrderbookLayout from "../../OrderbookLayout";
 import DocumentHead from "../../DocumentHead";
@@ -12,95 +9,14 @@ import NavMenu from "../NavMenu";
 import setBgImage from "../../../utils/setBgImage";
 import headerBanner from "../../../assets/images/headerBanner.png";
 
-export default function AllOffers() {
-	const pageName = "Investor";
-
-	const mockData = [
-		{
-			name: "Rice Chain Value",
-			tranche: "Tranche 1",
-			tenor: "90 days",
-			size: "NGN 10 Million",
-			status: "Not published",
-		},
-		{
-			name: "Agrix",
-			tranche: "Tranche 3",
-			tenor: "50 days",
-			size: "USD 1 Million",
-			status: "Published",
-		},
-		{
-			name: "Trinster startup",
-			tranche: "Tranche 3",
-			tenor: "50 days",
-			size: "NGN 1 Billion",
-			status: "Published",
-		},
-		{
-			name: "TribeModern Realty",
-			tranche: "Tranche 3",
-			tenor: "50 days",
-			size: "NGN 10 Million",
-			status: "Not published",
-		},
-	];
-
-	// Parameters
-	const eachPage = 4;
-
-	const [paginateState, setPaginateState] = useState({
-		list: mockData.length > 0 && mockData,
-		perPage: eachPage,
-		page: 0,
-		pages: Math.floor(mockData.length / eachPage),
-	});
-
-	const { page, perPage, pages, list } = paginateState;
-	let items = list.slice(page * perPage, (page + 1) * perPage);
-
-	const handlePageClick = (event) => {
-		let page = event.selected;
-		setPaginateState((state) => ({ ...state, page: page }));
-	};
+export default function InvestorDashboard() {
+	const pageName = "Investor"
 
 	return (
 		<>
 			<DocumentHead title={pageName} />
 			<OrderbookLayout PageNav={NavMenu}>
 				<section id="orderbook-investor-dashboard">
-					<div
-						id="loan-invest-dropdown"
-						class="bg-white px-16 py-10 shadow-md flex justify-start w-full"
-					>
-						<Link
-							to="/investor/dashboard"
-							id="home"
-							className="dropdown-container mr-5"
-						>
-							Home
-						</Link>
-						<Link to="/investor/offers/offer" id="offers" className="dropdown-container">
-							Offers
-						</Link>
-					</div>
-					<div
-						id="orderbook-dashboard-intro"
-						style={setBgImage(headerBanner)}
-					>
-						<h1>Hello, Ola</h1>
-						<h3>Welcome to your dashboard</h3>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit, sed do eiusmod tempor incididunt ut labore et
-							dolore magna aliqua. Ut enim ad minim veniam, quis
-							nostrud exercitation
-						</p>
-						<Button
-							title="Update your profile"
-							buttonClass="update-profile-cta"
-						/>
-					</div>
 					<div
 						id="user-quick-action"
 						className="flex flex-row justify-evenly h-20 w-full"
@@ -136,54 +52,16 @@ export default function AllOffers() {
 							</h2>
 						</div>
 					</div>
-					<div id="offer-status" className="w-full">
-						<div id="title" className="pb-2 pt-5">
-							<h3>My Offers</h3>
-						</div>
-						<table className="table-auto w-full">
-							<thead>
-								<tr>
-									<th>Name</th>
-									<th>Tranche</th>
-									<th>Tenor</th>
-									<th>Size</th>
-									<th>Status</th>
-								</tr>
-							</thead>
 
-							{items.map((item, index) => (
-								<tbody key={index}>
-									<tr>
-										<td className="offer-status-name">
-											<input type="checkbox" name="checkbox" className="checkbox mr-2 rounded" />
-											<span>{item.name}</span>
-										</td>
-										<td>{item.tranche}</td>
-										<td>{item.tenor}</td>
-										<td>{item.size}</td>
-										<td>
-											<Button title={item.status} buttonClass="bg-gray-500 rounded-md" />
-										</td>
-									</tr>
-								</tbody>
-							))}
-						</table>
-						
-						<hr className="border-1 border-white mt-10" />
-
-						{/*Pagination*/}
-						<div id="paginate-offer-status">
-							<ReactPaginate
-								previousLabel={"<"}
-								nextLabel={">"}
-								pageCount={pages}
-								containerClassName={"pagination"}
-								onPageChange={(e) => handlePageClick(e)}
-							/>
+					<div id="notifications-container">
+						<h3 className="text-3xl text-white font-bold pl-5 py-10">Notification</h3>
+						<div className="flex flex-col md:flex-row md:pr-5">
+							<div id="blank-space" className="bg-white hidden md:block">
+							</div>
 						</div>
 					</div>
 				</section>
 			</OrderbookLayout>
 		</>
-	);
+	)
 }
