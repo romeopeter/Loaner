6d16acac2473f3e4ecabba258fde0b5cdb1053e5
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-23 19:04:39 +0100

    Investor dashboard

--- src/components/dashboard/investor/Dashboard.js
+++ src/components/dashboard/investor/Dashboard.js
@@ -1,5 +1,8 @@
-import React, {createRef} from "react";
+import React, { createRef, useState } from "react";
+
 import { Link, useNavigate, Navigate } from "react-router-dom";
+
+import ReactPaginate from "react-paginate";
 
 import OrderbookLayout from "../../OrderbookLayout";
 import DocumentHead from "../../DocumentHead";
@@ -8,9 +11,28 @@ import NavMenu from "../NavMenu";
 
 import setBgImage from "../../../utils/setBgImage";
 import headerBanner from "../../../assets/images/headerBanner.png";
+import { offers } from "../../../fake-backend/investor/offers";
 
-export default function Dashboard() {
-	const pageName = "Investor"
+export default function AllOffers() {
+	const pageName = "Investor";
+
+	// Parameters
+	const eachPage = 9;
+
+	const [paginateState, setPaginateState] = useState({
+		list: offers.length > 0 && offers,
+		perPage: eachPage,
+		page: 0,
+		pages: Math.floor(offers.length / eachPage),
+	});
+
+	const { page, perPage, pages, list } = paginateState;
+	let items = list.slice(page * perPage, (page + 1) * perPage);
+
+	const handlePageClick = (event) => {
+		let page = event.selected;
+		setPaginateState((state) => ({ ...state, page: page }));
+	};
 
 	return (
 		<>
@@ -21,25 +43,31 @@ export default function Dashboard() {
 						id="loan-invest-dropdown"
 						class="bg-white px-16 py-10 shadow-md flex justify-start w-full"
 					>
-						<Link to="/investor/dashboard" id="home" className="dropdown-container mr-5">
+						<Link
+							to="/investor/dashboard"
+							id="home"
+							className="dropdown-container mr-5"
+						>
 							Home
 						</Link>
-						<Link to="/" id="offers" className="dropdown-container">
+						<Link to="/investor/offers/offer" id="offers" className="dropdown-container">
 							Offers
 						</Link>
 					</div>
-					<div id="orderbook-dashboard-intro" style={setBgImage(headerBanner)}>
+					<div
+						id="orderbook-dashboard-intro"
+						style={setBgImage(headerBanner)}
+					>
 						<h1>Hello, Ola</h1>
 						<h3>Welcome to your dashboard</h3>
 						<p>
 							Lorem ipsum dolor sit amet, consectetur adipiscing
 							elit, sed do eiusmod tempor incididunt ut labore et
 							dolore magna aliqua. Ut enim ad minim veniam, quis
-							nostrud exercitation.
+							nostrud exercitation
 						</p>
 						<Button
-							title="Update your profile"
-							link="/profile"
+							title="Update my profile"
 							buttonClass="update-profile-cta"
 						/>
 					</div>
@@ -78,70 +106,54 @@ export default function Dashboard() {
 							</h2>
 						</div>
 					</div>
+					<div id="offer-status" className="w-full">
+						<div id="title" className="pb-2 pt-5">
+							<h3>My Offers</h3>
+						</div>
+						<table className="table-auto w-full">
+							<thead>
+								<tr>
+									<th>Name</th>
+									<th>Tranche</th>
+									<th>Tenor</th>
+									<th>Size</th>
+									<th>Status</th>
+								</tr>
+							</thead>
 
-					<div id="notifications-container">
-						<h3 className="text-3xl text-white font-bold pl-5 py-10">Notification</h3>
-						<div className="flex flex-col md:flex-row md:pr-5">
-							<div id="notifications" className="pb-10">
-								<div className="notification notification-1">
-									<div className="badge">A</div>
-									<div>
-										<Link to="/investor/offers/offer">
-											Rice value chain ( Project Finance ) sent you an offer view more
-											{" "}
-											<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-										</Link>
-										<small>
-											1 day ago
-										</small>
-									</div>
-								</div>
-								<div className="notification notification-1">
-									<div className="badge">A</div>
-									<div>
-										<Link to="/">
-											Rice value chain ( Project Finance ) sent you an offer view more
-											{" "}
-											<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-										</Link>
-										<small>
-											1 day ago
-										</small>
-									</div>
-								</div>
-								<div className="notification notification-1">
-									<div className="badge">A</div>
-									<div>
-										<Link to="/">
-											Rice value chain ( Project Finance ) sent you an offer view more
-											{" "} 
-											<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-										</Link>
-										<small>
-											1 day ago
-										</small>
-									</div>
-								</div>
-								<div className="notification notification-1">
-									<div className="badge">A</div>
-									<div>
-										<Link to="/">
-											Rice value chain ( Project Finance ) sent you an offer view more
-											{" "}
-											<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-										</Link>
-										<small>
-											1 day ago
-										</small>
-									</div>
-								</div>
-							</div>
-							<div id="blank-space" className="bg-white hidden md:block">
-							</div>
+							{items.map((item, index) => (
+								<tbody key={index}>
+									<tr>
+										<td className="offer-status-name">
+											<input type="checkbox" name="checkbox" className="checkbox mr-2 rounded" />
+											<span>{item.name}</span>
+										</td>
+										<td>{item.tranche}</td>
+										<td>{item.tenor}</td>
+										<td>{item.size}</td>
+										<td>
+											<Button title={item.status} buttonClass="bg-gray-500 rounded-md" />
+										</td>
+									</tr>
+								</tbody>
+							))}
+						</table>
+						
+						<hr className="border-1 border-white mt-10" />
+
+						{/*Pagination*/}
+						<div id="paginate-offer-status">
+							<ReactPaginate
+								previousLabel={"<"}
+								nextLabel={">"}
+								pageCount={pages}
+								containerClassName={"pagination"}
+								onPageChange={(e) => handlePageClick(e)}
+							/>
 						</div>
 					</div>
 				</section>
 			</OrderbookLayout>
 		</>
-	)
+	);
 }
