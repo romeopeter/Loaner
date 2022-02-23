50946fd1dee23f6157bcebe90e14bfe3117c9867
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-23 03:41:57 +0100

    Fake backend

---
+++ src/fake-backend/broker/AllClients.js
@@ -0,0 +1,67 @@
+const AllClientsData = [
+    {
+        clientName: "Rice Value Chain",
+        clientType: "Investor",
+        category: "Private Organization",
+        org:"Private Organization",
+        details:"View more details"
+    },
+    {
+        clientName: "Rice Value Chain",
+        clientType: "Investor",
+        category: "Private Organization",
+        org:"Private Organization",
+        details:"View more details"
+    },
+    {
+        clientName: "Rice Value Chain",
+        clientType: "Investor",
+        category: "Private Organization",
+        org:"Private Organization",
+        details:"View more details"
+    },
+    {
+        clientName: "Rice Value Chain",
+        clientType: "Investor",
+        category: "Private Organization",
+        org:"Private Organization",
+        details:"View more details"
+    },
+    {
+        clientName: "Rice Value Chain",
+        clientType: "Investor",
+        category: "Private Organization",
+        org:"Private Organization",
+        details:"View more details"
+    },
+    {
+        clientName: "Rice Value Chain",
+        clientType: "Investor",
+        category: "Private Organization",
+        org:"Private Organization",
+        details:"View more details"
+    },
+    {
+        clientName: "Rice Value Chain",
+        clientType: "Investor",
+        category: "Private Organization",
+        org:"Private Organization",
+        details:"View more details"
+    },
+    {
+        clientName: "Rice Value Chain",
+        clientType: "Investor",
+        category: "Private Organization",
+        org:"Private Organization",
+        details:"View more details"
+    },
+    {
+        clientName: "Rice Value Chain",
+        clientType: "Investor",
+        category: "Private Organization",
+        org:"Private Organization",
+        details:"View more details"
+    },
+    
+]
+export default AllClientsData
