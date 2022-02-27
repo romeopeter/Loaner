5660e84c59587078b926743382e646f042a4fbf2
Romeo-Peter <romeomeshack34@gmail.com>
2022-02-28 00:47:08 +0100

    Declined bids for investors

---
+++ src/fake-backend/investor/declinedBids.js
@@ -0,0 +1,128 @@
+export const declinedBids = [
+		{
+			bidName: "Rice Value Chain",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"Rice is the most consumed commodity. Total global rice expenditure in 2020 was $350 billion. To put in perspective, totoal global crude oil in 2020 was just four times that amount at $1.3 trillion.",
+		},
+		{
+			bidName: "Rice Value Chain",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"Rice is the most consumed commodity. Total global rice expenditure in 2020 was $350 billion. To put in perspective, totoal global crude oil in 2020 was just four times that amount at $1.3 trillion.",
+		},
+		{
+			bidName: "Rice Value Chain",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"Rice is the most consumed commodity. Total global rice expenditure in 2020 was $350 billion. To put in perspective, totoal global crude oil in 2020 was just four times that amount at $1.3 trillion.",
+		},
+		{
+			bidName: "Rice Value Chain",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"Rice is the most consumed commodity. Total global rice expenditure in 2020 was $350 billion. To put in perspective, totoal global crude oil in 2020 was just four times that amount at $1.3 trillion.",
+		},
+		{
+			bidName: "Rice Value Chain",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"Rice is the most consumed commodity. Total global rice expenditure in 2020 was $350 billion. To put in perspective, totoal global crude oil in 2020 was just four times that amount at $1.3 trillion.",
+		},
+		{
+			bidName: "Rice Value Chain",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"Rice is the most consumed commodity. Total global rice expenditure in 2020 was $350 billion. To put in perspective, totoal global crude oil in 2020 was just four times that amount at $1.3 trillion.",
+		},
+		{
+			bidName: "Rice Value Chain",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"Rice is the most consumed commodity. Total global rice expenditure in 2020 was $350 billion. To put in perspective, totoal global crude oil in 2020 was just four times that amount at $1.3 trillion.",
+		},
+		{
+			bidName: "Rice Value Chain",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"Rice is the most consumed commodity. Total global rice expenditure in 2020 was $350 billion. To put in perspective, totoal global crude oil in 2020 was just four times that amount at $1.3 trillion.",
+		},
+		{
+			bidName: "Rice Value Chain",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"Rice is the most consumed commodity. Total global rice expenditure in 2020 was $350 billion. To put in perspective, totoal global crude oil in 2020 was just four times that amount at $1.3 trillion.",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+		{
+			bidName: "TribeModern Realty Transactions",
+			bidImage: "https://stockaru/skfd34-dfksdf-/ifksndfk3.jpg",
+			bidDescription:
+				"A real estate transaction is the process whereby rights in a unit of property (or designated real estate) is transferred between two or more parties, e.g. in case of conveyance one party being the seller(s) and the other being the buyer(s).",
+		},
+	];
