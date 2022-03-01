be51482be206b814dc69a42eaeda3784b716ce15
Romeo-Peter <romeomeshack34@gmail.com>
2022-03-01 02:39:19 +0100

    Alert components for feedback messages

---
+++ src/components/alert.js
@@ -0,0 +1,39 @@
+const Success = ({message}) => {
+	return (
+	<div className="mb-5">
+		<div className="text-black text-center bg-green-100 p-2 rounded border border-1 border-green-400">
+			{message}
+		</div>
+	</div>
+	)
+}
+
+const Danger = ({message}) => {
+	return (
+		<div className="mb-5">
+			<div className="text-black text-center bg-red-100 p-2 rounded border border-1 border-red-400">
+				{message}
+			</div>
+		</div>
+	)
+}
+const Warning = () => {
+	return (
+		<div className="mb-5">
+			<div className="text-black text-center bg-yellow-100 p-2 rounded border border-1 border-yellow-400">
+				{message}
+			</div>
+		</div>
+	)
+}
+const Info = () => {
+	return (
+		<div className="mb-5">
+			<div className="text-black text-center bg-blue-100 p-2 rounded border border-1 border-blue-400">
+				{message}
+			</div>
+		</div>
+	)
+}
+
+export {Success, Danger, Warning, Info}
