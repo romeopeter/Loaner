e19f031bae6ea5a9b162e26a8c7736e6ccf7034a
Romeo-Peter <romeomeshack34@gmail.com>
2022-03-01 11:03:15 +0100

    Feedback message alerts

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,6 +1,6 @@
 const Success = ({message}) => {
 	return (
-	<div className="mb-5">
+	<div className="my-2">
 		<div className="text-black text-center bg-green-100 p-2 rounded border border-1 border-green-400">
 			{message}
 		</div>
@@ -10,7 +10,7 @@ const Success = ({message}) => {
 
 const Danger = ({message}) => {
 	return (
-		<div className="mb-5">
+		<div className="my-2">
 			<div className="text-black text-center bg-red-100 p-2 rounded border border-1 border-red-400">
 				{message}
 			</div>
@@ -19,7 +19,7 @@ const Danger = ({message}) => {
 }
 const Warning = ({message}) => {
 	return (
-		<div className="mb-5">
+		<div className="my-2">
 			<div className="text-black text-center bg-yellow-100 p-2 rounded border border-1 border-yellow-400">
 				{message}
 			</div>
@@ -28,7 +28,7 @@ const Warning = ({message}) => {
 }
 const Info = ({message}) => {
 	return (
-		<div className="mb-5">
+		<div className="my-2">
 			<div className="text-black text-center bg-blue-100 p-2 rounded border border-1 border-blue-400">
 				{message}
 			</div>
