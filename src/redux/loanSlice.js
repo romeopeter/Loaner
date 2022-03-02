2d859fc6fac4c0cb3e60629d8d87ba1ba814ee59
Romeo-Peter <romeomeshack34@gmail.com>
2022-03-02 17:49:34 +0100

    Modified slice reducer

--- src/redux/loanSlice.js
+++ src/redux/loanSlice.js
@@ -6,55 +6,120 @@
 
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import { 
-    loanRequestCP, 
-    loanRequestBond, 
-    getOffers, 
-    loanRequestAddInvestor, 
-    loanRequestPublish 
+import {
+    loanRequestCP,
+    loanRequestBond,
+    getOffers,
+    loanRequestAddInvestor,
+    loanRequestPublish,
 } from "../services/loan.service.js";
 
 import { setServerMessage } from "./messageSlice";
 import handleRequestError from "./errorResponse";
 
-export const getOffersAction = createAsyncThunk("loan/getOffers", async (thunkAPI) => {
-    const res = await getOffers();
+export const CPLoanOfferAction = createAsyncThunk(
+    "loan/CPLoanOfferAction",
+    async (data, thunkAPI) => {
+        const res = await loanRequestCP(data);
+        const dispatch = thunkAPI.dispatch;
 
-    if (res.status === 200) return res.data;
-});
+        handleRequestError(res, dispatch);
 
-export const AddInvestorsAction = createAsyncThunk("loan/AddInvestors", async(id, data, thunkAPI) => {
-    const res = await loanRequestAddInvestor(id, data);
-    const dispatch = thunkAPI.dispatch;
+        if (res.status === 200 || res.status === 201) return res.data;
+    }
+);
+export const bondLoanOfferAction = createAsyncThunk(
+    "loan/bondLoanOfferAction",
+    async (data, thunkAPI) => {
+        const res = await loanRequestBond(data);
+        const dispatch = thunkAPI.dispatch;
 
-    handleRequestError(res, dispatch);
+        handleRequestError(res, dispatch);
 
-    if (res.status === 200) return res.data;
-})
+        if (res.status === 200 || res.status === 201) return res.data;
+    }
+);
 
-export const publishOfferAction = createAsyncThunk("loan/PublishOfferAction", async(id, thunkAPI) => {
-    const res = await loanRequestPublish(id);
-    const dispatch = thunkAPI.dispatch;
+export const getOffersAction = createAsyncThunk(
+    "loan/getOffers",
+    async (thunkAPI) => {
+        const res = await getOffers();
+        // const dispatch = thunkAPI.dispatch;
 
-    handleRequestError(res, dispatch);
+        // handleRequestError(res, dispatch);
 
-    if (res.status === 200) return res.data;
-})
+        if (res.status === 200) return res.data;
+    }
+);
+
+export const AddInvestorsAction = createAsyncThunk(
+    "loan/AddInvestors",
+    async (id, data, thunkAPI) => {
+        const res = await loanRequestAddInvestor(id, data);
+        const dispatch = thunkAPI.dispatch;
+
+        handleRequestError(res, dispatch);
+
+        if (res.status === 200 || res.status === 201) return res.data;
+    }
+);
+
+export const publishOfferAction = createAsyncThunk(
+    "loan/PublishOfferAction",
+    async (id, thunkAPI) => {
+        const res = await loanRequestPublish(id);
+        const dispatch = thunkAPI.dispatch;
+
+        handleRequestError(res, dispatch);
+
+        if (res.status === 200 || res.status === 201) return res.data;
+    }
+);
 
 export const loanSlice = createSlice({
     name: "loan",
-    initialState: {offers: []},
+    initialState: {
+        offers: [],
+        currentOffer: null,
+    },
     reducers: {
         createLoan: (state, action) => {
             return;
         },
     },
     extraReducers: {
+        // CREATE CP LOAN OFFERS
+        [CPLoanOfferAction.pending]: (state, action) => {
+            console.log("Pending");
+        },
+        [CPLoanOfferAction.rejected]: (state, action) => {
+            console.log("CP loan not created");
+        },
+        [CPLoanOfferAction.fulfilled]: (state, action) => {
+            console.log("Request fulfilled");
+            const payload = action.payload !== undefined && action.payload;
+            state.currentOffer = payload;
+        },
+
+        // CREATE BOND LOAN OFFERS
+        [bondLoanOfferAction.pending]: (state, action) => {
+            console.log("Pending");
+        },
+        [bondLoanOfferAction.rejected]: (state, action) => {
+            console.log("Bond loan not created");
+        },
+        [bondLoanOfferAction.fulfilled]: (state, action) => {
+            console.log("Request fulfilled");
+            const payload = action.payload !== undefined && action.payload;
+            state.currentOffer = payload;
+        },
+
+        // GET OFFERS
         [getOffersAction.pending]: (state, action) => {
-            console.log("Pending")
+            console.log("Pending");
         },
         [getOffersAction.rejected]: (state, action) => {
-            console.log("Rejected")
+            console.log("Rejected");
         },
         [getOffersAction.fulfilled]: (state, action) => {
             const payload = action.payload !== undefined && action.payload;
@@ -62,16 +127,15 @@ export const loanSlice = createSlice({
 
             console.log(state.offers);
         },
-    }
+    },
 });
 
 export default loanSlice.reducer;
 
 export const asyncCPLoanRequest = (data) => (dispatch) => {
     return loanRequestCP(data).then((response) => {
-
-        if(response.status === 200 || response.status === 201) {
-           dispatch(setServerMessage("Loan created succesfully"));
+        if (response.status === 200 || response.status === 201) {
+            dispatch(setServerMessage("Loan created succesfully"));
         }
 
         // Check error
@@ -79,15 +143,14 @@ export const asyncCPLoanRequest = (data) => (dispatch) => {
             dispatch(setServerMessage(response.message));
         }
 
-        return response
+        return response;
     });
 };
 
 export const asyncBondLoanRequest = (data) => (dispatch) => {
     return loanRequestBond(data).then((response) => {
-
-        if(response.status === 200 || response.status === 201) {
-           dispatch(setServerMessage("Loan created succesfully"));
+        if (response.status === 200 || response.status === 201) {
+            dispatch(setServerMessage("Loan created succesfully"));
         }
 
         // Check error
@@ -95,6 +158,6 @@ export const asyncBondLoanRequest = (data) => (dispatch) => {
             dispatch(setServerMessage(response.message));
         }
 
-        return response
+        return response;
     });
 };
