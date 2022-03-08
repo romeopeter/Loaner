8b92848c3d26e9cd4bf24a712aa82a6a535c4163
Romeo-Peter <romeomeshack34@gmail.com>
2022-03-08 13:46:06 +0100

    Modified extry file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import axios from "axios";
 
 import App from "./components/App";
 import store from "./redux/store";
@@ -11,15 +10,6 @@ import "./assets/styles/main.scss";
 
 import { positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
-
-// Axios configuration
-const currentUserObj = JSON.parse(localStorage.getItem("USER"));
-axios.defaults.baseURL = "https://order-book-online.herokuapp.com/";
-axios.defaults.headers.common["Authorization"] = undefined;
-if (currentUserObj !== null && currentUserObj !== false) {
-  axios.defaults.headers.common["Authorization"] = `Bearer ${currentUserObj.tokens.access}`;
-}
-axios.defaults.headers.post["Content-Type"] = "application/json";
 
 const options = {
   timeout: 5000,
