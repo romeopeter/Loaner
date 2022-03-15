03cf71a98176cee68b24fc52fac96edebbac12f7
Romeo-Peter <romeomeshack34@gmail.com>
2022-03-15 14:46:29 +0100

    Payment http request

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 const getPayments = () => {
-	retunr axios.get("/v1/payments/").catch(error => error);
+	return axios.get("/v1/payments/").catch(error => error);
 }
 
 const getPayment = (ID) => {
-	retunr axios.get(`/v1/payments/${ID}`).catch(error => error);
+	return axios.get(`/v1/payments/${ID}/`).catch(error => error);
 }
 
 const makePayment = () => {
 	return axios.post("/v1/payments/").catch(error => error)
 }
+
+export {getPayments, getPayment, makePayment}
