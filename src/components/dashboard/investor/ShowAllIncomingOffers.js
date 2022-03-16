84646aafdcab2217ff93b1a19f9445b63406a2f4
Romeo-Peter <romeomeshack34@gmail.com>
2022-03-16 17:35:49 +0100

    Sub components for AllOffers

---
+++ src/components/dashboard/investor/ShowAllIncomingOffers.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, Navigate } from "react-router-dom";
+import ReactPaginate from "react-paginate";
+import Button from "../../Button";
+
+import offerImage from "../../../assets/images/offerImage.png";
+
+export default function ShowAllIncomingOffers({incomingOffers}) {
+
+	// Pagination
+	const [paginateState, setPaginateState] = useState({
+		list: [],
+		perPage: 9,
+		page: 0,
+		pages: 0,
+	});
+
+	useEffect(() => {
+		if (incomingOffers !== null) {
+			setPaginateState({
+				list: incomingOffers,
+				perPage: 9,
+				page: 0,
+				pages: Math.floor(incomingOffers.length / 9)
+			});
+		}
+	}, [])
+
+	const { page, perPage, pages, list } = paginateState;
+	let items = list.slice(page * perPage, (page + 1) * perPage);
+
+	const handlePageClick = (event) => {
+		let page = event.selected;
+		setPaginateState((state) => ({ ...state, page: page }));
+	};
+
+	return (
+		<>
+			<div id="table-container" style={{ overflowX: "auto" }}>
+				<div id="table-action" className="bg-white py-5 px-2 w-full">
+					<select
+						name="table-action"
+						id="select-table-action"
+						className="mr-2 mt-1 focus:ring-white focus:border-black border-2 border-black"
+					>
+						<option defaultValue="value 1">Select action</option>
+						<option value="value 1">Option 1</option>
+						<option value="vallue 2">Option 2</option>
+						<option value="value 3">Option 3</option>
+					</select>
+					<Button
+						title="Apply"
+						buttonClass="bg-gray-500 action-btn"
+					/>
+				</div>
+				<table className="bg-white table-auto w-full">
+					<thead className="bg-gray-300">
+						<th className="pl-10 py-5 text-left">
+							<input
+								type="checkbox"
+								name="checkbox"
+								className="checkbox rounded mr-5"
+							/>
+							<span>Name</span>
+						</th>
+						<th className="pl-5 py-5" colspan="2">
+							Description
+						</th>
+					</thead>
+					<tbody>
+						{items.map((item, index) => {
+							return (
+								<tr key={index}>
+									<td className="offer-name">
+										<input
+											type="checkbox"
+											name="checkbox"
+											className="checkbox rounded"
+										/>
+										<img
+											src={offerImage}
+											alt=""
+											className="rounded h-10 w-10"
+										/>
+										<span>{item.bidName}</span>
+									</td>
+									<td>
+										<p>{item.bidDescription}</p>
+									</td>
+									<td>
+										<Button
+											title="View details"
+											buttonClass={`action-btn ${item.availability.replace(
+												" ",
+												"-"
+											)}`}
+										/>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+			<div id="paginate-offers" className="bg-white">
+				<ReactPaginate
+					previousLabel={"<"}
+					nextLabel={">"}
+					pageCount={pages}
+					containerClassName={"pagination"}
+					onPageChange={(e) => handlePageClick(e)}
+				/>
+			</div>
+		</>
+	)
+}
