f78288c24660e2db09ba48c01a9411f8be3e45b5
Romeo-Peter <romeomeshack34@gmail.com>
2022-03-22 20:26:45 +0100

    Upload of proof of payment

--- src/components/dashboard/investor/BidApproved.js
+++ src/components/dashboard/investor/BidApproved.js
@@ -6,8 +6,9 @@ import Button from "../../Button";
 import OrderbookLayout from "../../OrderbookLayout";
 import DocumentHead from "../../DocumentHead";
 import NavMenu from "../NavMenu";
-import {getAllOffersStatusAction} from "../../../redux/bidSlice";
+import {getAllOffersStatusAction, getBidAction} from "../../../redux/bidSlice";
 import { getInvestorAllOffersAction } from "../../../redux/investorSlice";
+import { uploadPaymentAction } from "../../../redux/paymentSlice";
 import { Danger } from "../../alert";
 
 
@@ -21,13 +22,17 @@ export default function BidApproved() {
 	const currentUserObj = useSelector((state) => state.auth.user);
 	const allOffers = useSelector(state => state.investor.allOffers);
 	const allBidsStatus = useSelector(state => state.bid.allBidsStatus);
+	const popFileDetails = JSON.parse(localStorage.getItem("INVESTOR_BID_POP_FILE"));
 	const urlParams = useParams();
-	const dispatch = useDispatch()
+	const dispatch = useDispatch();
 	const {id: investorId} = currentUserObj.user["investor_details"];
 
 	const [offer, setOffer] = useState(null);
 	const [bid, setBid] = useState(null);
 	const [fileState, setFileState] = useState({
+		fileName: null,
+		fileSize: null,
+		fileURL: null,
 		fileIsUploading: false,
 		fileIsUploaded: false,
 		formatIsWrong: false,
@@ -59,6 +64,7 @@ export default function BidApproved() {
 	}, [allOffers,investorId,urlParams.offerId,dispatch])
 
 	useEffect(function getBid() {
+		let isSubscribed = true;
 		if (allBidsStatus !== null) {
 
 			const offerId = urlParams.offerId;
@@ -70,13 +76,26 @@ export default function BidApproved() {
 					const bidMatchedOffer = offerBid["loan_request"]["id"] === Number(offerId);
 
 					if (bidMatchedOffer) setBid(offerBid);
-
 				}
 			})
-		}
-	});
+		} else {
+			(async function() {
+				const req = await dispatch(getBidAction(urlParams.offerId));
 
-	const handleFileUpload = (e) => {
+				if (req.meta.requestStatus === "fulfilled") {
+
+					const offerBid = req.payload[0];
+
+					if(isSubscribed) setBid(offerBid);
+				}
+			})()
+		}
+
+		return () => isSubscribed = false;
+
+	},[dispatch, urlParams.offerId]);
+
+	const handleFileUpload = async (e) => {
 		const popFile = e.target.files["0"];
 
 		const updateFileState = (updatedState) => {
@@ -93,7 +112,6 @@ export default function BidApproved() {
 			if (popFileFormat !== "jpg" && popFileFormat !== "pdf") {
 				updateFileState({formatIsWrong: true})
 			} else {
-				updateFileState({formatIsWrong: false, fileIsUploaded: true});
 				
 				const popFileUrl = URL.createObjectURL(popFile);
 				// const bidAmount = offer["tranche_id"]["size"]["minimum_subscription"]["amount"];
@@ -101,16 +119,37 @@ export default function BidApproved() {
 				const bidStatus = bid !== null && bid["current_status"];
 				const bidId = bid !== null && bid.id;
 
+				updateFileState({
+					fileName: popFileName,
+					fileSize: popfileSize,
+					fileURL: popFileUrl,
+					formatIsWrong: false, 
+					fileIsUploaded: true
+				});
+
 				// Send data
 				const reqData = {
 					pop_file_name: popFileName,
-					pop_fifle_url: popFileUrl,
+					pop_file_url: popFileUrl.split("blob:")[1],
 					amount: bidAmount,
 					status: bidStatus,
 					bid: bidId
 				}
 
-				console.log(reqData);
+				const dataIsSent = await dispatch(uploadPaymentAction(reqData));
+
+				if (dataIsSent.meta.requestStatus === "fulfilled") {
+					URL.revokeObjectURL(popFile)
+
+					// Save files details to browser
+					const popFileIsStored = localStorage.setItem(
+						"INVESTOR_BID_POP_FILE", 
+						JSON.stringify({popFileName, popfileSize, popFileUrl})
+					);
+
+					// Refreshe page to show POP confirmation update
+					if (popFileIsStored) window.location.reload();
+				};
 			}
 		}
 	}
@@ -141,7 +180,7 @@ export default function BidApproved() {
 										</div>
 										<h3 className="text-2xl underline text-center py-5 font-bold">{offer.deal_name}.</h3>
 
-										<div id="upload-area" className={!fileState.fileIsUploaded ? "block": "hidden"}>
+										<div id="upload-area" className={bid !== null && bid["payment_status"] === null ? "block": "hidden"}>
 											<p id="offer-description" className="text-green-700 pb-5 font-bold">
 											Your bid with {offer.deal_name} has been approved, 
 											click below to upload your payment proof.
@@ -155,29 +194,61 @@ export default function BidApproved() {
 												{fileState.formatIsWrong ? (<Danger message={fileState.formatErrorMessage} />): null}
 											</div>
 										</div>
-										<div id="upload-success" className={fileState.fileIsUploaded ? "block": "hidden"}>
-											<p className="text-center text-green-700 mb-5">Your file has been successfully uploaded.</p>
+										{(bid !== null && bid["payment_status"] !== null) ? (
+											<div id="upload-success">
+												<p className="text-center text-green-700 mb-5">Your file has been successfully uploaded.</p>
 
-											{/*Show uploaded file*/}
-											<div id="show-pop-file" className="mt-5">
-												<ul>
-													<li className="border-t-2">
-														<div className="flex justify-evenly">
-															<span className="font-bold text-gray-500">pop.jpg</span>
-															<span className="font-bold text-red-300 hover:text-red-500 text-xl cursor-pointer">&times;</span>
-														</div>
-													</li>
-												</ul>
+												{/*Show uploaded file*/}
+												<div id="show-pop-file" className="my-10">
+													<ul>
+														<li className="border-t-2 pt-2 mb-5">
+															<div className="flex justify-evenly">
+																<strong className="font-bold text-gray-700">Deal Name</strong>
+																<strong className="font-bold text-gray-700">File Name</strong>
+																<strong className="font-bold text-gray-700">Payment Status</strong>
+															</div>
+														</li>
+														<li className="border-b-2 pb-2">
+															<div className="flex justify-evenly">
+																<span className="text-left text-gray-400">{offer !== null && offer["deal_name"]}</span>
+																<span className="text-left text-gray-400" style={{textOverflowX: "auto"}}>{popFileDetails !== null && popFileDetails.popFileName}</span>
+																<>
+																	{bid["payment_status"] === "rejected" && (
+																		<Button 
+																			title="Not approved" 
+																			buttonClass="bg-red-400 rounded-md text-center payment-not-approved" 
+																			buttonDisabled={true}
+																		/>
+																	)}
+																	
+																	{bid["payment_status"] === "approved" && (
+																		<Button 
+																			title="Approved" 
+																			link={`/investor/dashboard/offers/${urlParams.offerId}/payment-detail`} buttonClass="bg-green-400 text-center rounded-md payment-approved"
+																		/>
+																	)}
+
+																	{bid["payment_status"] === "pending" && (
+																		<Button 
+																			title="Pending" 
+																			buttonClass="bg-yellow-400 text-center rounded-md payment-pending"
+																		/>
+																	)}
+																</>
+															</div>
+														</li>
+													</ul>
+												</div>
+
+												<div className="flex flex-col sm:flex-row justify-evenly items-center">
+													{/*White background, black borders*/}
+													<Button title="View offers" link="/investor/dashboard/offers" buttonClass="view-offers font-bold rounded sm:mb-0 mb-5" />
+
+													{/*Green background, white text*/}
+													<Button title="Go home" link="/investor/dashboard" buttonClass="go-home w-full font-bold rounded" />
+												</div>
 											</div>
-
-											<div className="flex flex-col sm:flex-row justify-evenly items-center">
-												{/*White background, black borders*/}
-												<Button title="View offers" link="/investor/dashboard/offers" buttonClass="view-offers font-bold rounded sm:mb-0 mb-5" />
-
-												{/*Green background, white text*/}
-												<Button title="Go home" link="/investor/dashboard" buttonClass="go-home w-full font-bold rounded" />
-											</div>
-										</div>
+										):null}
 									</div>
 								</>
 							)}	
