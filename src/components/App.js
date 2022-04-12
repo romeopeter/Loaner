4fde518dc665ee0acda47b16ac506a4353634071
romeo <romeomeshack34@gmail.com>
2022-04-13 00:11:05 +0100

    Added missing parenthesis to logic line

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,9 @@ import AppRoutes from "./AppRoutes";
 import AppErrorBoundary from "./AppErrorBoundary";
 
 import { signOutAsync } from "../redux/authSlice";
-import {setClientMessage} from "../redux/messageSlice.js";
+import { setClientMessage } from "../redux/messageSlice.js";
 
-function App() {
+export default function App() {
   const userObj = JSON.parse(localStorage.getItem("USER"));
   const dispatch = useDispatch();
 
@@ -21,45 +21,48 @@ function App() {
       "Authorization"
     ] = `Bearer ${userObj.tokens.access}`;
 
-  // Interceptor checks response for expire on invalid token
-  const requestsInterceptor = axios.interceptors.response.use(
-    () => {},
-    function (error) {
+    // Interceptor checks response for expire on invalid token
+    const requestsInterceptor = axios.interceptors.response.use(
+      () => {},
+      function (error) {
         const response = error.response;
 
         if (response.status === 401 && response.statusText === "Unauthorized") {
-            const message = dispatch(
-                setClientMessage({
-                    status: 401,
-                    messageType: "token_not_valid",
-                    message: "Unauthorized: Token is invalid or expired",
-                    detail: "Login token is expired. Please sign in",
-                })
-            );
+          const message = dispatch(
+            setClientMessage({
+              status: 401,
+              messageType: "token_not_valid",
+              message: "Unauthorized: Token is invalid or expired",
+              detail: "Login token is expired. Please sign in",
+            })
+          );
 
-            const messageObj = typeof message.payload === "object" && message.payload
+          const messageObj =
+            typeof message.payload === "object" && message.payload;
 
-            if (messageObj !== false) {
-                if (messageObj.messageType !== undefined && messageObj.messageType === "token_not_valid") {
-                    // Sign user out
-                    dispatch(signOutAsync());
-                    
-                    // Redirect login page
-                    // navigate("/login");
-                }
+          if (messageObj !== false) {
+            if (
+              messageObj.messageType !== undefined &&
+              messageObj.messageType === "token_not_valid"
+            ) {
+              // Sign user out
+              dispatch(signOutAsync());
+
+              // Redirect login page
+              // navigate("/login");
             }
+          }
         }
-    }
-  );
+      }
+    );
 
     // Remove interceptors
-  axios.interceptors.response.eject(requestsInterceptor)
+    axios.interceptors.response.eject(requestsInterceptor);
 
-  return (
-    <AppErrorBoundary>
-      <AppRoutes />
-    </AppErrorBoundary>
-  );
+    return (
+      <AppErrorBoundary>
+        <AppRoutes />
+      </AppErrorBoundary>
+    );
+  }
 }
-
-export default App;
