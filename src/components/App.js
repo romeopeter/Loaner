b86470669e0c65b434111482753b5d4424c2f743
romeo <romeomeshack34@gmail.com>
2022-04-13 09:53:17 +0100

    Modified

--- src/components/App.js
+++ src/components/App.js
@@ -8,61 +8,61 @@ import { signOutAsync } from "../redux/authSlice";
 import { setClientMessage } from "../redux/messageSlice.js";
 
 export default function App() {
-  const userObj = JSON.parse(localStorage.getItem("USER"));
-  const dispatch = useDispatch();
+    const userObj = JSON.parse(localStorage.getItem("USER"));
+    const dispatch = useDispatch();
 
-  // Set default endpoint URL and Authorization key
-  axios.defaults.baseURL = "https://order-book-online.herokuapp.com/";
-  axios.defaults.headers.post["Content-Type"] = "application/json";
-  axios.defaults.headers.common["Authorization"] = undefined;
+    // Set default endpoint URL and Authorization key
+    axios.defaults.baseURL = "https://order-book-online.herokuapp.com/";
+    axios.defaults.headers.post["Content-Type"] = "application/json";
+    axios.defaults.headers.common["Authorization"] = undefined;
 
-  if (userObj !== null && userObj !== false) {
-    axios.defaults.headers.common[
-      "Authorization"
-    ] = `Bearer ${userObj.tokens.access}`;
+    if (userObj !== null && userObj !== false) {
+        axios.defaults.headers.common[
+            "Authorization"
+        ] = `Bearer ${userObj.tokens.access}`;
 
-    // Interceptor checks response for expire on invalid token
-    const requestsInterceptor = axios.interceptors.response.use(
-      () => {},
-      function (error) {
-        const response = error.response;
+        // Interceptor checks response for expire on invalid token
+        const requestsInterceptor = axios.interceptors.response.use(
+            () => { },
+            function (error) {
+                const response = error.response;
 
-        if (response.status === 401 && response.statusText === "Unauthorized") {
-          const message = dispatch(
-            setClientMessage({
-              status: 401,
-              messageType: "token_not_valid",
-              message: "Unauthorized: Token is invalid or expired",
-              detail: "Login token is expired. Please sign in",
-            })
-          );
+                if (response.status === 401 && response.statusText === "Unauthorized") {
+                    const message = dispatch(
+                        setClientMessage({
+                            status: 401,
+                            messageType: "token_not_valid",
+                            message: "Unauthorized: Token is invalid or expired",
+                            detail: "Login token is expired. Please sign in",
+                        })
+                    );
 
-          const messageObj =
-            typeof message.payload === "object" && message.payload;
+                    const messageObj =
+                        typeof message.payload === "object" && message.payload;
 
-          if (messageObj !== false) {
-            if (
-              messageObj.messageType !== undefined &&
-              messageObj.messageType === "token_not_valid"
-            ) {
-              // Sign user out
-              dispatch(signOutAsync());
+                    if (messageObj !== false) {
+                        if (
+                            messageObj.messageType !== undefined &&
+                            messageObj.messageType === "token_not_valid"
+                        ) {
+                            // Sign user out
+                            dispatch(signOutAsync());
 
-              // Redirect login page
-              // navigate("/login");
+                            // Redirect login page
+                            // navigate("/login");
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
-    );
+        );
 
-    // Remove interceptors
-    axios.interceptors.response.eject(requestsInterceptor);
+        // Remove interceptors
+        axios.interceptors.response.eject(requestsInterceptor);
+    }
 
     return (
-      <AppErrorBoundary>
-        <AppRoutes />
-      </AppErrorBoundary>
+        <AppErrorBoundary>
+            <AppRoutes />
+        </AppErrorBoundary>
     );
-  }
 }
