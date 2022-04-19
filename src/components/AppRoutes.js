f74835b75343731fab7d84862fb784c5880c504e
romeo <romeomeshack34@gmail.com>
2022-04-19 14:49:06 +0100

    Modified PublishOffer routes

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -129,7 +129,7 @@ export default function AppRoutes() {
                     />
 
                     <Route
-                        path='/client/offers/offer/publish/:id/::dealType'
+                        path='/client/offers/offer/publish/:id/:dealType'
                         element={
                             <RequireAuth>
                                 <PublishOffer />
