ff1ae7918d368ff3a5cf21386e698939e4d9d79b
romeo <romeomeshack34@gmail.com>
2022-04-24 13:33:53 +0100

    Added http request to get approved and declined bids

--- src/components/dashboard/investor/SuccessfulOffers.js
+++ src/components/dashboard/investor/SuccessfulOffers.js
@@ -1,38 +1,68 @@
 import React, { useState, useEffect } from "react";
-
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import ReactPaginate from "react-paginate";
 
 import OrderbookLayout from "../../OrderbookLayout";
 import DocumentHead from "../../DocumentHead";
 import Button from "../../Button";
 import NavMenu from "../NavMenu";
-
 import offerImage from "../../../assets/images/offerImage.png";
 
-import {successfulBids} from "../../../fake-backend/investor/successfulBids"
+import { getApprovedBidsAction } from "../../../redux/bidSlice";
+import { successfulBids } from "../../../fake-backend/investor/successfulBids";
 
 export default function SuccessfulBids() {
 	const pageName = "Successful offers";
+	const dispatch = useDispatch();
 
-	// Pagination
-	const eachPage = 9;
-
+	/* Pagination stars */
 	const [paginateState, setPaginateState] = useState({
-		list: successfulBids.length > 0 && successfulBids,
-		perPage: eachPage,
+		list: [],
+		perPage: 9,
 		page: 0,
-		pages: Math.floor(successfulBids.length / eachPage),
+		pages: 0,
 	});
 
 	const { page, perPage, pages, list } = paginateState;
-	let items = list.slice(page * perPage, (page + 1) * perPage);
+	let approvedBids = list.slice(page * perPage, (page + 1) * perPage);
 
 	const handlePageClick = (event) => {
 		let page = event.selected;
 		setPaginateState((state) => ({ ...state, page: page }));
 	};
+	/* Pagination Ends */
+
+	const [isLoading, setisLoading] = useState(true);
+	// const [approvedBids, setapprovedBids] = useState(null);
+
+	useEffect(function getapprovedBids() {
+		let componentIsMounted = true;
+
+		(async () => {
+			const req = await dispatch(getApprovedBidsAction());
+
+			// setisLoading(true);
+
+			if (req.meta.requestStatus === "fulfilled") {
+				if (componentIsMounted) {
+					setPaginateState(state => ({
+						...state, 
+						list: req.payload,
+						pages: Math.floor(successfulBids.length / 9)
+					}));
+					setisLoading(false);
+				};
+			}
+
+			componentIsMounted = false;
+		})();
+
+		console.log(approvedBids);
+
+		return () => componentIsMounted;
+
+	}, [dispatch])
 
 	return (
 		<>
@@ -59,12 +89,19 @@ export default function SuccessfulBids() {
 				</div>
 				<section id="orderbook-investor-successful-bids">
 					<div id="Succesful-bids">
-						<h3 id="header" className="py-10 text-lg sm:text-2xl pl-5 text-white">Succesful offers</h3>
+						<h3 id="header" className="py-10 text-lg sm:text-2xl pl-5 text-white">Succesful Bids</h3>
+						{isLoading ? (<div className="bg-white w-full h-auto">
+							<p className="text-3xl text-center h-40 flex justify-center items-center text-gray-500">Loading {" "}{" "} 
+								<i className="fa fa-spinner fa-pulse fa-3x fa-fw"
+								style={{ fontSize: 20 }}></i>
+							</p>
+						</div>) : (
 						<div id="table-container" style={{ overflowX: "auto" }}>
+
 							<div id="table-action" className="bg-white py-5 px-2 w-full">
-								<select 
-									name="table-action" 
-									id="select-table-action" 
+								<select
+									name="table-action"
+									id="select-table-action"
 									className="mr-2 mt-1 focus:ring-white focus:border-black border-2 border-black"
 								>
 									<option defaultValue="value 1">Select action</option>
@@ -77,9 +114,9 @@ export default function SuccessfulBids() {
 
 							<table className="bg-white table-auto w-full">
 								<tbody>
-									{items.map((item, index) => {
+									{approvedBids.map((item, index) => {
 										return (
-											<tr key={index}>
+											<tr key={item.id}>
 												<td className="offer-name">
 													<div>
 														<input
@@ -93,11 +130,11 @@ export default function SuccessfulBids() {
 															alt=""
 															className="rounded h-10 w-10 hidden sm:block"
 														/>
-														<span>{item.bidName}</span>
+														<span>{item["loan_request"]["tranche_name"]}</span>
 													</div>
 												</td>
 												<td className="offer-description">
-													<p className="">{item.bidDescription}</p>
+													<p className="text-center" style={{textAlign: "center"}}>{item["loan_request"]["tranche_name"]}{" "}description</p>
 												</td>
 												<td className="table-btn">
 													<Button
@@ -110,8 +147,9 @@ export default function SuccessfulBids() {
 									})}
 								</tbody>
 							</table>
-						</div>
-						<div id="paginate-bids" className="bg-white">
+						</div>)}
+						{approvedBids.length > 0 && (
+							<div id="paginate-bids" className="bg-white">
 							<ReactPaginate
 								previousLabel={"<"}
 								nextLabel={">"}
@@ -119,7 +157,9 @@ export default function SuccessfulBids() {
 								containerClassName={"pagination"}
 								onPageChange={(e) => handlePageClick(e)}
 							/>
-						</div>
+							</div>
+						)}
+						
 					</div>
 				</section>
 			</OrderbookLayout>
