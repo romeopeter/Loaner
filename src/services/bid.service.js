1366f07b9b2a5fddd8cc31544f9d1da968adccc2
romeo <romeomeshack34@gmail.com>
2022-04-24 13:34:48 +0100

    Added http request to get approved and declined bids

--- src/services/bid.service.js
+++ src/services/bid.service.js
@@ -18,9 +18,25 @@ const updateBids = (id) => {
         .catch((error) => console.log(error));
 };
 
+const getapprovedBids = () => {
+    return axios.get("/v1/bids/?status=approved").catch(err => err);
+}
+
+const getDeclinedBids = () => {
+    return axios.get("/v1/bids/?status=declined").catch(err => err);
+}
+
 const getAllOffersStatus = (requestsArr) => {
     return axios.all(requestsArr.map(req => axios.get(req)))
     .then(responses => responses.map(res => res.data)).catch(error => error);
 }
 
-export { getBids, getBid, createBid, updateBids, getAllOffersStatus }; 
+export { 
+    getBids, 
+    getBid, 
+    createBid, 
+    updateBids, 
+    getapprovedBids, 
+    getDeclinedBids, 
+    getAllOffersStatus 
+}; 
