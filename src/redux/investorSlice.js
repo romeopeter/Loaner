89afeae9f1b4a3c73782ef859d1ead32afff2440
romeo <romeomeshack34@gmail.com>
2022-05-17 13:43:59 +0100

    ---

--- src/redux/investorSlice.js
+++ src/redux/investorSlice.js
@@ -1,7 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import {getInvestorAllOffers} from "../services/loan.service.js";
-import { setServerMessage } from "./messageSlice";
-import handleRequestError from "./errorResponse";
+import {getInvestorAllOffers, getOffer} from "../services/loan.service.js";
+import handleRequestError from "./errorHandler";
 
 export const getInvestorAllOffersAction = createAsyncThunk(
 	"investor/getInvestorAllOffersAction",
@@ -10,21 +9,49 @@ export const getInvestorAllOffersAction = createAsyncThunk(
 		const res = await getInvestorAllOffers(ID);
 
 		// Catch error
-		handleRequestError(res, dispatch);
+		if (res.status !== 200) handleRequestError(res, dispatch);
 
 		if (res.status === 200) return res.data
 	}
+);
+
+export const getOfferAction = createAsyncThunk(
+    "loan/getOfferAction",
+    async (requestParams, thunkAPI) => {
+        const {id, dealType} = requestParams;
+
+        const res = await getOffer(dealType, id);
+        const dispatch = thunkAPI.dispatch;
+
+        if (res.status !== 200) handleRequestError(res, dispatch);
+
+        if (res.status === 200) return res.data;
+    }
 );
 
 export const investorSlce = createSlice({
 	name: "investor",
 	initialState: {
 		allOffers: null,
+		currentOffer: null,
 		declinedOffers: null,
 		successfulOffers: null,
 	},
 	reducers: "",
 	extraReducers: {
+		// Get offers
+        [getOfferAction.pending]: (state, action) => {
+            console.log("Pending");
+        },
+        [getOfferAction.rejected]: (state, action) => {
+            console.log("Rejected");
+        },
+        [getOfferAction.fulfilled]: (state, action) => {
+            const payload = action.payload !== undefined && action.payload;
+            state.currentOffer = payload;
+        },
+
+		// Get all investor's offers
 		[getInvestorAllOffersAction.pending]: () => {
 			console.log("Pending");
 		},
