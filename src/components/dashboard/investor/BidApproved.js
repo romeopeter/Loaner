a396e51235243e21995bc9bd6117d6e4ceec0898
romeo <romeomeshack34@gmail.com>
2022-05-30 19:21:34 +0100

    Modified component

--- src/components/dashboard/investor/BidApproved.js
+++ src/components/dashboard/investor/BidApproved.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link, useParams } from "react-router-dom";
-import Button from "../../Button";
+import { Link, useParams, useNavigate } from "react-router-dom";
 
 import OrderbookLayout from "../../OrderbookLayout";
 import DocumentHead from "../../DocumentHead";
 import NavMenu from "../NavMenu";
+
 import { getBidAction } from "../../../redux/bidSlice";
 import { getInvestorAllOffersAction } from "../../../redux/investorSlice";
 import { uploadPaymentAction } from "../../../redux/paymentSlice";
@@ -20,9 +20,10 @@ export default function BidApproved() {
   const currentUserObj = useSelector((state) => state.auth.user);
   const allBidsStatus = useSelector((state) => state.bid.allBidsStatus);
   const urlParams = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { id: investorId } = currentUserObj.user["investor_details"];
 
+  const { id: investorId } = currentUserObj.user["investor_details"];
   const [offer, setOffer] = useState(null);
   const [bid, setBid] = useState(null);
   const [fileState, setFileState] = useState({
@@ -68,12 +69,12 @@ export default function BidApproved() {
           if (bid !== undefined && bid.length > 0) {
             const offerBid = bid[0];
 
-            console.log(offerBid);
-
             if (componentIsMounted) {
               const bidMatchedOffer =
                 offerBid["loan_request"]["id"] === Number(offerId);
               if (bidMatchedOffer) setBid(offerBid);
+
+              console.log(bid);
             }
           }
         });
@@ -95,6 +96,7 @@ export default function BidApproved() {
   );
 
   const handleFileUpload = async (e) => {
+
     const popFile = e.target.files["0"];
 
     const updateFileState = (updatedState) => {
@@ -136,199 +138,112 @@ export default function BidApproved() {
           bid: bidId,
         };
 
-        const dataIsSent = await dispatch(uploadPaymentAction(reqData));
+        const req = await dispatch(uploadPaymentAction(reqData));
+        const reqFulfilled = req.meta.requestStatus;
+        const paymentStatus = req.payload.payment.status;
 
-        if (dataIsSent.meta.requestStatus === "fulfilled") {
+        if (reqFulfilled === "fulfilled" && paymentStatus !== null) {
           URL.revokeObjectURL(popFile);
 
-          // Refreshe page to show POP confirmation update
-          window.location.reload();
+          // Redirect to BAPaymentProof component
+          navigate(`/investor/dashboard/${urlParams.offerId}/payment-proof`);
         }
       }
     }
   };
 
-  // Proof of payment details
-  let paymentStatus;
-  let proofOfPayment;
-  let popFileName;
+  if (bid !== null && bid.payment.status !== null) {
+    // Redirect to BAPaymentProof component
+    navigate(`/investor/dashboard/${urlParams.offerId}/payment-proof`);
+  }
 
-  if (bid !== null) {
-    paymentStatus = bid["payment"]["status"];
-    proofOfPayment = bid["payment"]["proof_of_payment"][0];
-    popFileName = proofOfPayment["file_name"];
+
+  if (offer !== null) {
+    return (
+      <>
+        <DocumentHead title={pageName} />
+        <OrderbookLayout PageNav={NavMenu}>
+          <section id="orderbook-bid-approved" className="bg-gray-800 sm:p-20">
+            <h2
+              id="offer-title"
+              className="text-white font-bold mb-10 text-2xl pt-5 pl-5 sm:pt-0 sm:pl-0 sm:text-3xl"
+            >
+              {offer !== null && offer.deal_name}
+            </h2>
+            <div className="bg-white" id="bid-container">
+              <div id="go-back-arrow" className="bg-gray-300 mb-5">
+                <Link
+                  to="/investor/dashboard"
+                  className="flex justify-center items-center"
+                >
+                  <i className="fa fa-long-arrow-left" aria-hidden="true"></i>
+                </Link>
+              </div>
+              <div
+                id="inner-bid-container"
+                className="flex flex-col justify-center items-center p-5 sm:p-0"
+              >
+                <div id="bid" className="mb-5">
+                  <div id="img-container">
+                    <img src={offerImage} alt="" className="w-full" />
+                    <div className="overlay">
+                      <div
+                        id="tick-icon"
+                        className="bg-white h-20 w-20 rounded-full flex justify-center items-center"
+                      >
+                        <img src={accepted} alt="accepted-tick-marked" />
+                      </div>
+                    </div>
+                  </div>
+                  <h3 className="text-2xl underline text-center py-5 font-bold">
+                    {offer.deal_name}.
+                  </h3>
+
+                  <div id="upload-area block">
+                    <p
+                      id="offer-description"
+                      className="text-green-700 pb-5 font-bold"
+                    >
+                      Your bid with {offer.deal_name} has been approved, click
+                      below to upload your payment proof.
+                    </p>
+                    <div className="flex flex-col justify-center items-center">
+                      <label
+                        htmlFor="upload-payment"
+                        id="upload-label"
+                        className="font-bold py-2 px-10 bg-gray-300 cursor-pointer text-center"
+                      >
+                        <input
+                          type="file"
+                          id="upload-payment"
+                          className="hidden"
+                          onChange={(e) => handleFileUpload(e)}
+                        />
+                        Select file to upload
+                      </label>
+                      <small className="text-center text-blue-500 p-2">
+                        File should be either jpeg or pdf.
+                      </small>
+                      {fileState.formatIsWrong ? (
+                        <Danger message={fileState.formatErrorMessage} />
+                      ) : null}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </OrderbookLayout>
+      </>
+    );
   }
 
   return (
-    <>
-      <DocumentHead title={pageName} />
-      <OrderbookLayout PageNav={NavMenu}>
-        <section id="orderbook-bid-approved" className="bg-gray-800 sm:p-20">
-          <h2
-            id="offer-title"
-            className="text-white font-bold mb-10 text-2xl pt-5 pl-5 sm:pt-0 sm:pl-0 sm:text-3xl"
-          >
-            {offer !== null && offer.deal_name}
-          </h2>
-          <div className="bg-white" id="bid-container">
-            <div id="go-back-arrow" className="bg-gray-300 mb-5">
-              <Link
-                to="/investor/dashboard"
-                className="flex justify-center items-center"
-              >
-                <i className="fa fa-long-arrow-left" aria-hidden="true"></i>
-              </Link>
-            </div>
-            <div
-              id="inner-bid-container"
-              className="flex flex-col justify-center items-center p-5 sm:p-0"
-            >
-              {offer === null ? (
-                <p>Loading...</p>
-              ) : (
-                <>
-                  <div id="bid" className="mb-5">
-                    <div id="img-container">
-                      <img src={offerImage} alt="" className="w-full" />
-                      <div className="overlay">
-                        <div
-                          id="tick-icon"
-                          className="bg-white h-20 w-20 rounded-full flex justify-center items-center"
-                        >
-                          <img src={accepted} alt="accepted-tick-marked" />
-                        </div>
-                      </div>
-                    </div>
-                    <h3 className="text-2xl underline text-center py-5 font-bold">
-                      {offer.deal_name}.
-                    </h3>
-
-                    <div
-                      id="upload-area"
-                      className={
-                        bid !== null && bid["payment_status"] === null
-                          ? "block"
-                          : "hidden"
-                      }
-                    >
-                      <p
-                        id="offer-description"
-                        className="text-green-700 pb-5 font-bold"
-                      >
-                        Your bid with {offer.deal_name} has been approved, click
-                        below to upload your payment proof.
-                      </p>
-                      <div className="flex flex-col justify-center items-center">
-                        <label
-                          htmlFor="upload-payment"
-                          id="upload-label"
-                          className="font-bold py-2 px-10 bg-gray-300 cursor-pointer text-center"
-                        >
-                          <input
-                            type="file"
-                            id="upload-payment"
-                            className="hidden"
-                            onChange={(e) => handleFileUpload(e)}
-                          />
-                          Select file to upload
-                        </label>
-                        <small className="text-center text-blue-500 p-2">
-                          File should be either jpeg or pdf
-                        </small>
-                        {fileState.formatIsWrong ? (
-                          <Danger message={fileState.formatErrorMessage} />
-                        ) : null}
-                      </div>
-                    </div>
-
-                    {bid !== null ? (
-                      <div id="upload-success">
-                        <p className="text-center text-green-700 mb-5">
-                          Your file has been successfully uploaded.
-                        </p>
-
-                        {/*Show uploaded file*/}
-                        <div id="show-pop-file" className="my-10">
-                          <ul>
-                            <li className="border-t-2 pt-2 mb-5">
-                              <div className="flex justify-evenly">
-                                <strong className="font-bold text-gray-700">
-                                  Deal Name
-                                </strong>
-                                <strong className="font-bold text-gray-700">
-                                  File Name
-                                </strong>
-                                <strong className="font-bold text-gray-700">
-                                  Payment Status
-                                </strong>
-                              </div>
-                            </li>
-                            <li className="border-b-2 pb-2">
-                              <div className="flex justify-evenly">
-                                <span className="text-left text-gray-400">
-                                  {offer !== null && offer["deal_name"]}
-                                </span>
-                                <span
-                                  className="text-left text-gray-400"
-                                  style={{ textOverflowX: "auto" }}
-                                >
-                                  {popFileName}
-                                </span>
-                                <>
-                                  {paymentStatus === "rejected" && (
-                                    <Button
-                                      title="Not approved"
-                                      buttonClass="bg-red-400 rounded-md text-center payment-not-approved"
-                                      buttonDisabled={true}
-                                    />
-                                  )}
-
-                                  {paymentStatus === "approved" && (
-                                    <Button
-                                      title="Approved"
-                                      link={`/investor/dashboard/offers/${urlParams.offerId}/payment-detail`}
-                                      buttonClass="bg-green-400 text-center rounded-md payment-approved"
-                                    />
-                                  )}
-
-                                  {paymentStatus === "pending" && (
-                                    <Button
-                                      title="Pending"
-                                      buttonClass="bg-yellow-400 text-center rounded-md payment-pending"
-                                    />
-                                  )}
-                                </>
-                              </div>
-                            </li>
-                          </ul>
-                        </div>
-
-                        <div className="flex flex-col sm:flex-row justify-evenly items-center">
-                          {/*White background, black borders*/}
-                          <Button
-                            title="View offer"
-                            link={`/investor/dashboard/offers/${
-                              offer["id"]
-                            }/${offer["deal_type"].toLowerCase()}`}
-                            buttonClass="view-offers font-bold rounded sm:mb-0 mb-5"
-                          />
-
-                          {/*Green background, white text*/}
-                          <Button
-                            title="Go home"
-                            link="/investor/dashboard"
-                            buttonClass="go-home w-full font-bold rounded"
-                          />
-                        </div>
-                      </div>
-                    ) : null}
-                  </div>
-                </>
-              )}
-            </div>
-          </div>
-        </section>
-      </OrderbookLayout>
-    </>
+    <div className="flex items-center justify-center" style={{height: "100vh"}}>
+      <p className="text-center text-gray-400 font-medium text-3xl">
+        Loading {" "}
+        <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+      </p>
+    </div>
   );
 }
