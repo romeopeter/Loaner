5cd10f3f9e4e90b690a095b8821ab00a06866384
romeo <romeomeshack34@gmail.com>
2022-05-31 23:31:27 +0100

    HTTP auth request

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,15 +2,13 @@ import axios from "axios";
 
 /*Authentication requests*/
 const signUpRequest = (data) => {
-	return axios
-		.post("v1/registration/sign_up/", data)
-		.catch((error) => error);
+  return axios.post("v1/registration/sign_up/", data).catch((error) => {
+    return error;
+  });
 };
 
 const signInRequest = (data) => {
-	return axios
-		.post("/v1/registration/sign_in/", data)
-		.catch((error) => error);
+  return axios.post("/v1/registration/sign_in/", data).catch((error) => error);
 };
 
-export { signUpRequest, signInRequest};
+export { signUpRequest, signInRequest };
