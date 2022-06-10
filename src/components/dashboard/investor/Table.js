179c3584ad21fad3ec5c21191d5da00deafde2fc
romeo <romeomeshack34@gmail.com>
2022-06-10 13:40:00 +0100

    Table

---
+++ src/components/dashboard/investor/Table.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { useTable } from "react-table";
+
+import Button from "../../Button";
+import offerImage from "../../../assets/images/offerImage.png";
+
+export default function Table({ columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
+  return (
+    <table className="bg-white table-fixed w-full" {...getTableProps()}>
+      <thead className="bg-gray-300">
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>
+                {column.render("header") === "TableBtn" ? (
+                  " "
+                ) : column.render("header") === "Name" ? (
+                  <div className="w-full">
+                    <input
+                      type="checkbox"
+                      name="checkbox"
+                      className="checkbox rounded float-left ml-5"
+                      title="checkbox"
+                    />
+                    <h6 className="inline-block">{column.render("header")}</h6>
+                  </div>
+                ) : (
+                 <h6>{ column.render("header")}</h6>
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps}>
+        {rows.map((row, index) => {
+          prepareRow(row);
+
+          return (
+            <tr {...row.getRowProps()} key={index}>
+              {row.cells.map((cell, index) => {
+                return (
+             
+                    <td
+                      {...cell.getCellProps()}
+                      className={`${
+                        cell.column.header === "Name"
+                          ? "offer-name"
+                          : cell.column.header === "TableBtn"
+                          ? "offer-btn"
+                          : "offer-description"
+                      }`}
+
+                      key={index}
+                    >
+                      {cell.column.header === "Name" ? (
+                        <div className="flex items-center">
+                          <input
+                            type="checkbox"
+                            name="checkbox"
+                            className="checkbox rounded"
+                            title="checkbox"
+                          />
+                          <img
+                            src={offerImage}
+                            alt=""
+                            className="rounded h-10 w-10 hidden sm:block"
+                          />
+                          <h6>{cell.render("Cell")}</h6>
+                        </div>
+                      ) : (
+                        ""
+                      )}
+
+                      {cell.column.header === "Description" ? (
+                        <p className="text-left">{cell.render("Cell")}</p>
+                      ) : (
+                        ""
+                      )}
+
+                      {cell.column.header === "TableBtn" ? (
+                        <div className="flex justify-center items-center">
+                          <Button
+                            title={cell.render("Cell")}
+                            link="#"
+                            buttonClass="bg-green-600 rounded hover:bg-white successful-btn"
+                          />
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                    </td>
+                 
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
