ca102421a3eca8ff7cf92d2835c0cf26d0185a0c
romeo <romeomeshack34@gmail.com>
2022-06-15 17:14:16 +0100

    Modified

--- src/redux/messageSlice.js
+++ src/redux/messageSlice.js
@@ -6,33 +6,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const messageSlice = createSlice({
-	name: "message",
-	initialState: {
-		server: { 
-			status: "",
-			messageType: "",
-			detail: "" 
-		},
-		client: {
-			status: "",
-			messageType: "",
-			detail: ""
-		},
-	},
-	reducers: {
-		setServerMessage: (state, action) => {
-			state.server.message = action.payload;
-		},
-		setClientMessage: (state, action) => {
-			state.client = action.payload;
-		},
-		clearServerMessage: (state, action) => {
-			state.message.server.message = "";
-		},
-		clearClientMessage: (state, action) => {
-			state.message.client = null;
-		},
-	},
+  name: "message",
+  initialState: {
+    server: {
+      status: "",
+      messageType: "",
+      detail: "",
+    },
+    client: {
+      status: "",
+      messageType: "",
+      detail: "",
+    },
+  },
+  reducers: {
+    setServerMessage: (state, action) => {
+      state.server.message = action.payload;
+    },
+    setClientMessage: (state, action) => {
+      state.client = action.payload;
+
+      localStorage.setItem(
+        "ORDERBOOK_EXPIRED_TOKEN",
+        JSON.stringify(state.client)
+      );
+    },
+    clearServerMessage: (state, action) => {
+      state.message.server.message = "";
+    },
+    clearClientMessage: (state, action) => {
+      state.message.client = null;
+    },
+  },
 });
 
 export const { setServerMessage, setClientMessage } = messageSlice.actions;
