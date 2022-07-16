d6df4394bc3c1d875b496da748dcd63ef181af41
romeo <romeomeshack34@gmail.com>
2022-07-16 13:00:39 +0100

    modified

---
+++ src/data/investor/declinedBids.js
@@ -0,0 +1,45 @@
+export const declinedBids = [
+    {
+        "id": 1,
+        "owner": {
+            "id": 18,
+            "first_name": "Romeo",
+            "last_name": "Peter",
+            "groups": [
+                {
+                    "name": "Investor"
+                }
+            ]
+        },
+        "loan_request": {
+            "id": 1,
+            "deal_type": "Commercial Paper",
+            "tranche_name": "Tranche 1",
+            "duration": 8,
+            "duration_interval": "days"
+        },
+        "current_status": "approved",
+        "status_updates": [
+            {
+                "name": "approved",
+                "message": "Please take heed to this"
+            }
+        ],
+        "payment": {
+            "id": 1,
+            "proof_of_payment": [
+                {
+                    "id": 1,
+                    "file_name": "pop.jpeg",
+                    "file_url": "http://localhost:3000/07bd422e-e98f-4e5b-b0de-c518e90def6d"
+                }
+            ],
+            "created_at": "2022-05-28T22:44:41.819286Z",
+            "modified_at": "2022-07-13T20:46:19.890614Z",
+            "amount": "56066.00",
+            "status": "approved",
+            "bid": 1
+        },
+        "amount": "56066.00"
+    }
+]
