3c8e17862b1c746d596be72c673278113c5025db
romeo <romeomeshack34@gmail.com>
2022-07-18 13:50:07 +0100

    Loan offer

--- src/components/dashboard/broker/LoanRequests.js
+++ src/components/dashboard/broker/LoanRequests.js
@@ -88,8 +88,6 @@ const AllLoanRequests = () => {
                             );
                                 })
                             }
-                    
-                            
                         </Tbody>
                         
                     </Table>
