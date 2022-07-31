cbaf78f68099362099ba066d181d9a4fdfb45900
romeo <romeomeshack34@gmail.com>
2022-07-31 11:35:22 +0100

    Payment table

--- src/components/dashboard/broker/payment/Table.js
+++ src/components/dashboard/broker/payment/Table.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Flex, Box, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 
 export default function RenderTable(props) {
-    const {
-        state, 
-        tableData, 
-        disableApproved, 
-        handleCheckFunc, 
-        openPaymentModalFunc, 
-        openModalApprovedFunc,
-        openModalRejectedFunc
-    } = props
+  const {
+    state,
+    tableData,
+    filterAction,
+    disableApproved,
+    handleCheckFunc,
+    openPaymentModalFunc,
+    openModalApprovedFunc,
+    openModalRejectedFunc,
+  } = props;
 
   return (
     <Table size="sm" colorScheme={"blackAlpha"}>
@@ -37,99 +38,104 @@ export default function RenderTable(props) {
       </Thead>
 
       <Tbody>
-          {/* CurrentTableData */}
+        {/* CurrentTableData */}
         {tableData.map((data, index) => {
           const bidPayment = data.payment;
 
-          return (
-            <Tr key={index}>
-              <Td></Td>
-              <Td>
-                {" "}
-                <input
-                  name={data.id}
-                  type="checkbox"
-                  className="broker-checkbox"
-                  disabled={data.payment_status === "approved"}
-                  // checked when checkedBid contains checked object
-                  checked={
-                    data.payment_status !== "approved" &&
-                    state.markedBids.some((item) => item?.id === data.id)
-                  }
-                  onChange={(e) => handleCheckFunc(e, data)}
-                />
-              </Td>
-              <Td>
-                <Flex>
-                  <Box
-                    w="40px"
-                    h="40px"
-                    borderRadius={"50%"}
-                    bg={"#555555"}
-                    mr={[4]}
-                  ></Box>
-                  <Flex alignSelf={"center"}>
-                    {data.owner.first_name} {data.owner.last_name}
+          if (
+            data["current_status"] === filterAction ||
+            filterAction === "Filter payment"
+          ) {
+            return (
+              <Tr key={index}>
+                <Td></Td>
+                <Td>
+                  {" "}
+                  <input
+                    name={data.id}
+                    type="checkbox"
+                    className="broker-checkbox"
+                    disabled={data.payment_status === "approved"}
+                    // checked when checkedBid contains checked object
+                    checked={
+                      data.payment_status !== "approved" &&
+                      state.markedBids.some((item) => item?.id === data.id)
+                    }
+                    onChange={(e) => handleCheckFunc(e, data)}
+                  />
+                </Td>
+                <Td>
+                  <Flex>
+                    <Box
+                      w="40px"
+                      h="40px"
+                      borderRadius={"50%"}
+                      bg={"#555555"}
+                      mr={[4]}
+                    ></Box>
+                    <Flex alignSelf={"center"}>
+                      {data.owner.first_name} {data.owner.last_name}
+                    </Flex>
                   </Flex>
-                </Flex>
-              </Td>
+                </Td>
 
-              <Td>{data.amount}</Td>
-              <Td>
-                {data.current_status.charAt(0).toUpperCase() +
-                  data.current_status.slice(1)}
-              </Td>
-
-              {bidPayment.status !== "" ? (
-                <Td color={"#008060"}>
-                  {bidPayment.status.charAt(0).toUpperCase() +
+                <Td>{data.amount}</Td>
+                <Td>
+                  {data.current_status.charAt(0).toUpperCase() +
                     data.current_status.slice(1)}
                 </Td>
-              ) : (
-                <Td color={"#eed202"}>Pending</Td>
-              )}
 
-              {bidPayment["proof_of_payment"].length > 0 ? (
-                <Td
-                  style={{
-                    cursor: "pointer",
-                    color: "#1C6CA6",
-                  }}
-                  onClick={() => openPaymentModalFunc(data)}
-                >
-                  View Payment
-                </Td>
-              ) : (
-                <Td>-</Td>
-              )}
+                {bidPayment.status !== "" ? (
+                  <Td color={"#008060"}>
+                    {bidPayment.status.charAt(0).toUpperCase() +
+                      data.current_status.slice(1)}
+                  </Td>
+                ) : (
+                  <Td color={"#eed202"}>Pending</Td>
+                )}
 
-              {data.payment_status ? (
-                <Td>
-                  <button disabled={true} className="payment-cta--approve">
-                    Approve Payment
-                  </button>
-
-                  <button disabled={true} className="payment-cta--reject">
-                    Reject Payment
-                  </button>
-                </Td>
-              ) : (
-                <Td className="payment-cta">
-                  <button
-                    onClick={() => openModalApprovedFunc(data)}
-                    className="payment-cta--approve rounded"
-                  >
-                    Approve Payment
-                  </button>
-
-                  <button
-                    onClick={() => {
-                        openModalRejectedFunc(data);
+                {bidPayment["proof_of_payment"].length > 0 ? (
+                  <Td
+                    style={{
+                      cursor: "pointer",
+                      color: "#1C6CA6",
                     }}
-                    className="payment-cta--reject rounded"
+                    onClick={() => openPaymentModalFunc(data)}
                   >
-                    Reject Payment
-                  </button>
+                    View Payment
+                  </Td>
+                ) : (
+                  <Td>-</Td>
+                )}
+
+                {bidPayment.status === "approved" && (
+                  <Td className="payment-cta">
+                    <button 
+                      disabled={true} className="payment-cta--approve rounded"  
+                      onClick={() => openModalApprovedFunc(data)}
+                    >
+                      Approve Payment
+                    </button>
+
+                    <button 
+                     disabled={true} className="payment-cta--reject rounded"
+                     onClick={() => {
+                      openModalRejectedFunc(data);
+                    }}
+                    >
+                      Reject Payment
+                    </button>
+                  </Td>
+                )}
+              </Tr>
+            );
+          }
+
+          return (
+            <Tr key={index}>
+              {index === 0 && (
+                <Td colSpan={7} className="text-center py-5 text-xl">
+                  You have no {filterAction} payment
                 </Td>
               )}
             </Tr>
