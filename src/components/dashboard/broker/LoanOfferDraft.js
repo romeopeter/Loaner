dac0eee921f00bde0070c2fc56a343504fec3165
romeo <romeomeshack34@gmail.com>
2022-08-01 16:50:28 +0100

    Modified side-effect

--- src/components/dashboard/broker/LoanOfferDraft.js
+++ src/components/dashboard/broker/LoanOfferDraft.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 import OrderbookLayout from '../../OrderbookLayout';
 import DocumentHead from '../../DocumentHead';
