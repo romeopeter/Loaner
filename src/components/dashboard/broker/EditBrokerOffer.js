0ab98c7898ec24471e06bd9978c0bb26ad80d251
romeo <romeomeshack34@gmail.com>
2022-08-01 19:42:47 +0100

    Modified

--- src/components/dashboard/broker/EditBrokerOffer.js
+++ src/components/dashboard/broker/EditBrokerOffer.js
@@ -16,6 +16,7 @@ import { cp, bond } from "../loan-request-data/requestData";
 import { getOfferAction, editOfferAction } from "../../../redux/loanSlice";
 
 import ShowLoanSummary from "./modals/ShowLoanSummary";
+import SubNavBar from "./layouts/SubNavBar";
 
 export default function EditBrokerOffer() {
   const pageName = "Loan offer edit";
@@ -86,8 +87,6 @@ export default function EditBrokerOffer() {
       scale: "",
     },
   });
-
-  const [isOpen, setOpen] = useState({ client: false, investor: false });
 
   const handleSubmit = async () => {
     const { user: currentUser } = JSON.parse(localStorage.getItem("USER"));
@@ -312,79 +311,14 @@ export default function EditBrokerOffer() {
     setShowModal(true);
   };
 
-  /* Dropdown starts */
-    const toggleDropdownClient = () => {
-      if (isOpen.client) {
-        setOpen({ ...isOpen, client: false });
-        return;
-      }
-
-      setOpen({ investor: false, client: true });
-    };
-
-    const toggleDropdownInvestor = () => {
-      if (isOpen.investor) {
-        setOpen({ ...isOpen, investor: false });
-        return;
-      }
-
-      setOpen({ client: false, investor: true });
-    };
-  /* Dropdown starts */
-
   return (
     <>
       <DocumentHead title={pageName} />
       <OrderbookLayout PageNav={NavMenu}>
         <section id="orderbook-loan-request">
-          <div className=" bg-white px-16 py-10 shadow-md flex justify-start">
-            <div className="dropdownbroker">
-              <div
-                className="dropdownbroker-header"
-                onClick={toggleDropdownClient}
-              >
-                <h2 className="mr-2">Clients</h2>
-                <i
-                  className={`fa fa-caret-down ${isOpen.client && "open"}`}
-                ></i>
-              </div>
-              <div className={`dropdownbroker-body ${isOpen.client && "open"}`}>
-                <Link
-                  to="/broker/dashboard/new-client"
-                  className="dropdownbroker-item "
-                >
-                  New Client{" "}
-                </Link>
-                <Link
-                  to="/broker/dashboard/allclients"
-                  className="dropdownbroker-item "
-                >
-                  Manage Clients{" "}
-                </Link>
-              </div>
-            </div>
-            <div className="dropdownbroker">
-              <div
-                className="dropdownbroker-header"
-                onClick={toggleDropdownInvestor}
-              >
-                <h2 className="mr-2">Investors</h2>
-                <i
-                  className={`fa fa-caret-down ${isOpen.investor && "open"}`}
-                ></i>
-              </div>
-              <div
-                className={`dropdownbroker-body ${isOpen.investor && "open"}`}
-              >
-                <Link
-                  to="/broker/dashboard/uploadInvestor"
-                  className="dropdownbroker-item "
-                >
-                  Upload Investors{" "}
-                </Link>
-              </div>
-            </div>
-          </div>
+          {/* Sub navbar */}
+          <SubNavBar />
+          
           <div
             id="loan-request-container"
             className=""
