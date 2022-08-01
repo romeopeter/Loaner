aebc66316d56c3b76f9ef2ed9f9a8c43c2370fda
romeo <romeomeshack34@gmail.com>
2022-08-01 16:50:07 +0100

    Modified side-effect

--- src/components/dashboard/broker/LoanOfferDraft.js
+++ src/components/dashboard/broker/LoanOfferDraft.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 import OrderbookLayout from '../../OrderbookLayout';
 import DocumentHead from '../../DocumentHead';
 import NavMenu from '../NavMenu';
+
+import { getOffer } from '../../../services/loan.service';
+import SubNavBar from './layouts/SubNavBar';
 import Arrow from '../../../assets/images/Arrow.png';
 import offerImage from '../../../assets/images/offerImage.png';
 
@@ -14,52 +18,28 @@ const LoanOfferDraft = () => {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        axios.get('/v1/loan_request/').then((response) => {
-            let index = response.data.findIndex((loan) => loan.id === parseInt(id));
-            setData(response.data[index]);
-        });
         window.scroll(0, 0);
-    }, [id]);
-    console.log(data);
 
-    // Dropdown
-    const [isOpen, setOpen] = useState({ client: false, investor: false });
-    const toggleDropdownClient = () =>
-        isOpen.client ? setOpen({ ...isOpen, client: false }) : setOpen({ investor: false, client: true });
-    const toggleDropdownInvestor = () =>
-        isOpen.investor ? setOpen({ ...isOpen, investor: false }) : setOpen({ client: false, investor: true });
+        let componentIsMounted = true;
+
+        (async function getLoanOffer() {
+            const res = await getOffer(dealType, id);
+      
+            if (res.statusText === "OK") {
+             if (componentIsMounted) setData(res.data);
+            }
+        })();
+
+        return () => componentIsMounted = false;
+    }, [id, dealType]);
 
     return (
         <>
             <DocumentHead title='Loan Draft' />
             <OrderbookLayout PageNav={NavMenu}>
-                <div className=' bg-white px-16 py-10 shadow-md flex justify-start'>
-                    <div className='dropdownbroker'>
-                        <div className='dropdownbroker-header' onClick={toggleDropdownClient}>
-                            <h2 className='mr-2'>Clients</h2>
-                            <i className={`fa fa-caret-down ${isOpen.client && 'open'}`}></i>
-                        </div>
-                        <div className={`dropdownbroker-body ${isOpen.client && 'open'}`}>
-                            <Link to='/broker/dashboard/new-client' className='dropdownbroker-item '>
-                                New Client{' '}
-                            </Link>
-                            <Link to='/broker/dashboard/allclients' className='dropdownbroker-item '>
-                                Manage Clients{' '}
-                            </Link>
-                        </div>
-                    </div>
-                    <div className='dropdownbroker'>
-                        <div className='dropdownbroker-header' onClick={toggleDropdownInvestor}>
-                            <h2 className='mr-2'>Investors</h2>
-                            <i className={`fa fa-caret-down ${isOpen.investor && 'open'}`}></i>
-                        </div>
-                        <div className={`dropdownbroker-body ${isOpen.investor && 'open'}`}>
-                            <Link to='/broker/dashboard/uploadInvestor' className='dropdownbroker-item '>
-                                Upload Investors{' '}
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+                {/* Sub navbar */}
+                <SubNavBar />
+
                 {!data ? (
                     <p className='loader' style={{ margin: '100px auto' }}></p>
                 ) : (
@@ -108,28 +88,16 @@ const LoanOfferDraft = () => {
                                             Description
                                         </p>
                                         <p
+                                            className="pb-3"
                                             style={{
                                                 textAlign: 'justify',
-                                                height: '250px !important',
+                                                height: 200,
+                                                overflowY: "scroll",
                                                 fontSize: '15px',
                                                 lineHeight: '2',
                                             }}
                                         >
-                                            Rice is the most consumed commodity. Total global rice expenditure in 2020
-                                            was $350 billion. To put in perspective, total global crude oil in 2020 was
-                                            just four times that amount at $1.3 trillion. Rice is the most consumed
-                                            commodity. Total global rice expenditure in 2020 was $350 billion. To put in
-                                            perspective, totoal global crude oil in 2020 was just four times that amount
-                                            at $1.3 trillion. Rice is the most consumed commodity. Total global rice
-                                            expenditure in 2020 was $350 billion. To put in perspective, totoal global
-                                            crude oil in 2020 was just four times that amount at $1.3 trillion. Rice is
-                                            the most consumed commodity. Total global rice expenditure in 2020 was $350
-                                            billion. To put in perspective, totoal global crude oil in 2020 was just
-                                            four times that amount at $1.3 trillion. Rice is the most consumed
-                                            commodity. Total global rice expenditure in 2020 was $350 billion. To put in
-                                            perspective, totoal global crude oil in 2020 was just four times that amount
-                                            at $1.3 trillion. Rice is the most consumed commodity. Total global rice
-                                            expenditure in 2020 was $350 billion.
+                                            {data.description !== null ? data.description : "No description"}
                                         </p>
 
                                         <Flex
@@ -149,7 +117,7 @@ const LoanOfferDraft = () => {
                                             </Box>
                                             <Box>
                                                 <p style={{ fontSize: '15px' }}>Loan amount</p>
-                                                <p style={{ fontSize: '16px', fontWeight: 'bold' }}>NGN 5 billion</p>
+                                                <p style={{ fontSize: '16px', fontWeight: 'bold' }}>{Math.round(data.tranche_id.size.minimum_subscription.amount)}</p>
                                             </Box>
                                             <Box mx={[0, 10]} my={[10, 0]}>
                                                 <p style={{ fontSize: '15px' }}>Tranche</p>
@@ -213,12 +181,13 @@ const LoanOfferDraft = () => {
                                                 borderRadius={'0'}
                                                 w={'120px'}
                                                 mt={['10px']}
+                                                mr={["10px"]}
                                                 as={Link}
                                                 to={`/broker/dashboard/edit-loan-offer/${id}/${dealType.toLocaleLowerCase()}`}
                                             >
                                                 Edit draft
                                             </Button>
-                                            <Button
+                                            {/* <Button
                                                 bg={'#C4C4C4'}
                                                 px={['6']}
                                                 color={'#000'}
@@ -231,7 +200,7 @@ const LoanOfferDraft = () => {
                                                 to='/broker/dashboard/all-offers/'
                                             >
                                                 Save draft
-                                            </Button>
+                                            </Button> */}
                                             <Button
                                                 bg={'#008060'}
                                                 px={['6']}
