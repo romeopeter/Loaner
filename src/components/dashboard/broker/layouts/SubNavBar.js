731abe7393d58b6d00682f3cf3ea6ba39640003e
romeo <romeomeshack34@gmail.com>
2022-08-01 12:16:43 +0100

    Sub nav

--- src/components/dashboard/broker/layouts/SubNavBar.js
+++ src/components/dashboard/broker/layouts/SubNavBar.js
@@ -18,55 +18,37 @@ export default function SubNavBar() {
   };
 
   return (
-    <div className=' bg-white px-16 py-10 shadow-md flex justify-start'>
-              <div className='dropdownbroker'>
-                <div
-                  className='dropdownbroker-header'
-                  onClick={toggleDropdownClient}
-                >
-                  <h2 className='mr-2'>Clients</h2>
-                  <i
-                    className={`fa fa-caret-down ${isOpen.client && 'open'}`}
-                  ></i>
-                </div>
-                <div
-                  className={`dropdownbroker-body ${isOpen.client && 'open'}`}
-                >
-                  <Link
-                    to='/broker/dashboard/new-client'
-                    className='dropdownbroker-item '
-                  >
-                    New Client{' '}
-                  </Link>
-                  <Link
-                    to='/broker/dashboard/allclients'
-                    className='dropdownbroker-item '
-                  >
-                    Manage Clients{' '}
-                  </Link>
-                </div>
-              </div>
-              <div className='dropdownbroker'>
-                <div
-                  className='dropdownbroker-header'
-                  onClick={toggleDropdownInvestor}
-                >
-                  <h2 className='mr-2'>Upload</h2>
-                  <i
-                    className={`fa fa-caret-down ${isOpen.investor && 'open'}`}
-                  ></i>
-                </div>
-                <div
-                  className={`dropdownbroker-body ${isOpen.investor && 'open'}`}
-                >
-                  <Link
-                    to='/broker/dashboard/upload'
-                    className='dropdownbroker-item '
-                  >
-                  Bulk upload {' '}
-                  </Link>
-                </div>
-              </div>
-            </div>
+    <div
+      id="sub-navbar"
+      className="bg-white px-16 py-10 shadow-md flex justify-start"
+    >
+      <div className="dropdownbroker navbar-item">
+        <div className="dropdownbroker-header" onClick={toggleDropdownClient}>
+          <h2 className="mr-2 underline">Clients</h2>
+          <i className={`fa fa-caret-down ${isOpen.client && "open"}`}></i>
+        </div>
+
+        <div className={`dropdownbroker-body ${isOpen.client && "open"}`}>
+          <Link
+            to="/broker/dashboard/new-client"
+            className="dropdownbroker-item "
+          >
+            New Client{" "}
+          </Link>
+          <Link
+            to="/broker/dashboard/allclients"
+            className="dropdownbroker-item "
+          >
+            Manage Clients{" "}
+          </Link>
+        </div>
+      </div>
+
+      <div className="navbar-item">
+        <h2 className="mr-2 underline">
+          <Link to="/broker/dashboard/upload">Upload users</Link>
+        </h2>
+      </div>
+    </div>
   );
 }
