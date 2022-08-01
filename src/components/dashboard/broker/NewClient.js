375b6f60c8a0d54b1ffa7c0e59758ae49c2d69e3
romeo <romeomeshack34@gmail.com>
2022-08-01 12:35:40 +0100

    Modified

--- src/components/dashboard/broker/NewClient.js
+++ src/components/dashboard/broker/NewClient.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect } from 'react';
 import OrderbookLayout from '../../OrderbookLayout';
 import DocumentHead from '../../DocumentHead';
 import NavMenu from '../NavMenu';
 import { Link } from 'react-router-dom';
+import SubNavBar from './layouts/SubNavBar';
 
 import { Flex, Box, FormControl, FormLabel, Input, Select, Heading, Button } from '@chakra-ui/react';
 
@@ -10,44 +11,13 @@ const NewClient = () => {
     useEffect(() => {
         window.scroll(0, 0);
     }, []);
-    // Dropdown
-    const [isOpen, setOpen] = useState({ client: false, investor: false });
-    const toggleDropdownClient = () =>
-        isOpen.client ? setOpen({ ...isOpen, client: false }) : setOpen({ investor: false, client: true });
-    const toggleDropdownInvestor = () =>
-        isOpen.investor ? setOpen({ ...isOpen, investor: false }) : setOpen({ client: false, investor: true });
 
     return (
         <>
             <DocumentHead title='New Client' />
             <OrderbookLayout PageNav={NavMenu}>
-                <div className=' bg-white px-16 py-10 shadow-md flex justify-start'>
-                    <div className='dropdownbroker'>
-                        <div className='dropdownbroker-header' onClick={toggleDropdownClient}>
-                            <h2 className='mr-2'>Clients</h2>
-                            <i className={`fa fa-caret-down ${isOpen.client && 'open'}`}></i>
-                        </div>
-                        <div className={`dropdownbroker-body ${isOpen.client && 'open'}`}>
-                            <Link to='/broker/dashboard/new-client' className='dropdownbroker-item '>
-                                New Client{' '}
-                            </Link>
-                            <Link to='/broker/dashboard/allclients' className='dropdownbroker-item '>
-                                Manage Clients{' '}
-                            </Link>
-                        </div>
-                    </div>
-                    <div className='dropdownbroker'>
-                        <div className='dropdownbroker-header' onClick={toggleDropdownInvestor}>
-                            <h2 className='mr-2'>Investors</h2>
-                            <i className={`fa fa-caret-down ${isOpen.investor && 'open'}`}></i>
-                        </div>
-                        <div className={`dropdownbroker-body ${isOpen.investor && 'open'}`}>
-                            <Link to='/broker/dashboard/uploadInvestor' className='dropdownbroker-item '>
-                                Upload Investors{' '}
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+                <SubNavBar />
+
                 <main>
                     <Flex flexDirection={'column'} bg='#555' px={['4.2%']} py={[10]}>
                         <Heading as='h1' size='lg' mb='10' color={'#fff'}>
