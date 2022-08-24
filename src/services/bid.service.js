8c2327b3ac0570d230284f05c62840eadd85d9fe
romeo <romeomeshack34@gmail.com>
2022-08-24 10:08:37 +0100

    modified

--- src/services/bid.service.js
+++ src/services/bid.service.js
@@ -1,47 +1,54 @@
 import axios from "axios";
 
 const getBids = () => {
-    return axios.get("/v1/bids/").catch(error => error);
+  return axios.get("/v1/bids/").catch((error) => error);
 };
 
 const getBid = (id) => {
-    return axios.get(`/v1/bids/?loan_request_id=${id}`);
+  return axios.get(`/v1/bids/?loan_request_id=${id}`);
 };
 
 const createBid = (data) => {
-    return axios.post("/v1/bids/", data);
+  return axios.post("/v1/bids/", data);
 };
-    
+
 const updateBids = (id) => {
-    return axios
-        .put(`/v1/payments/${id}/`)
-        .catch(error => error);
+  return axios.put(`/v1/payments/${id}/`).catch((error) => error);
 };
 
 const getapprovedBids = () => {
-    return axios.get("/v1/bids/?status=approved").catch(err => err);
-}
+  return axios.get("/v1/bids/?status=approved").catch((err) => err);
+};
 
 const getapprovedBid = (id) => {
-    return axios.get(`/v1/bids/?loan_request=${id}&status=approved`);
-}
+  return axios.get(`/v1/bids/?loan_request=${id}&status=approved`);
+};
 
 const getDeclinedBids = () => {
-    return axios.get("/v1/bids/?status=declined").catch(err => err);
-}
+  return axios.get("/v1/bids/?status=declined").catch((err) => err);
+};
 
 const getAllOffersStatus = (requestsArr) => {
-    return axios.all(requestsArr.map(req => axios.get(req)))
-    .then(responses => responses.map(res => res.data)).catch(error => error);
-}
+  return axios
+    .all(requestsArr.map((req) => axios.get(req)))
+    .then((responses) => responses.map((res) => res.data))
+    .catch((error) => error);
+};
 
-export { 
-    getBids, 
-    getBid, 
-    createBid, 
-    updateBids,
-    getapprovedBid, 
-    getapprovedBids, 
-    getDeclinedBids, 
-    getAllOffersStatus 
-}; 
+const rejectManualListingBids = (requestArr, rejectedMessage) => {
+  return axios
+    .all(requestArr.map((req) => axios.patch(req,rejectedMessage)))
+    .then((responses) => responses.map((res) => console.log(res)));
+};
+
+export {
+  getBids,
+  getBid,
+  createBid,
+  updateBids,
+  getapprovedBid,
+  getapprovedBids,
+  getDeclinedBids,
+  getAllOffersStatus,
+  rejectManualListingBids,
+};
