ac0fc6a91d9cb021f275b61055edd71d2c2c8842
romeo <romeomeshack34@gmail.com>
2022-08-26 11:24:04 +0100

    Batch bids reject modal

---
+++ src/components/dashboard/broker/modals/BatchRejectBidsModal.js
@@ -0,0 +1,33 @@
+import bidRejected from "../../../../assets/images/bidRejected.png";
+
+export default function BatchRejectBidsModal({
+  closeModal,
+  trigger,
+  notification,
+}) {
+  const className = trigger.showModal ? "open" : "";
+
+  const rejectedBidsNum =
+    trigger.rejectedBids !== null && trigger.rejectedBids.length;
+
+  return (
+    <div className={`modal ${className}`}>
+      <div className="modal-overlay" onClick={closeModal}></div>
+
+      <div className="modal-body h-60 flex justify-center">
+      {trigger.isLoading ? (  <p className="loader"></p>): null}
+
+        {!trigger.isLoading && (
+          <div>
+            <img alt="rejected" className="img" src={bidRejected} />
+            <h1 className="h1Rejected">Bids Rejected</h1>
+            <p>You rejected {rejectedBidsNum} bid offers.</p>
+            <button className="modal-button " onClick={closeModal}>
+              Back to list
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
