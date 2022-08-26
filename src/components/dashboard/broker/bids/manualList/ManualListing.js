eaec369f85fcd8e2c5c1b62a27b547546961b3ea
romeo <romeomeshack34@gmail.com>
2022-08-26 11:01:59 +0100

    Manual Listing

--- src/components/dashboard/broker/bids/manualList/ManualListing.js
+++ src/components/dashboard/broker/bids/manualList/ManualListing.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useCallback } from "react";
+import React, { useMemo } from "react";
 
 import AllBids from "./AllBids";
 import { humanNumber } from "../../../../../utils/HRN";
