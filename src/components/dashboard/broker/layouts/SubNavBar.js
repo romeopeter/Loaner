c343b9c812820ca9338c81f7e0d9c91398c443d9
romeo <romeomeshack34@gmail.com>
2022-09-14 17:42:44 +0100

    Added breadcrum navigation

--- src/components/dashboard/broker/layouts/SubNavBar.js
+++ src/components/dashboard/broker/layouts/SubNavBar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function SubNavBar() {
+export default function SubNavBar({ breadCrumb }) {
   // Dropdown
   const [isOpen, setOpen] = useState({ client: false, investor: false });
 
@@ -18,37 +18,87 @@ export default function SubNavBar() {
   };
 
   return (
-    <div
-      id="sub-navbar"
-      className="bg-white px-16 py-10 shadow-md flex justify-start"
-    >
-      <div className="dropdownbroker navbar-item">
-        <div className="dropdownbroker-header" onClick={toggleDropdownClient}>
-          <h2 className="mr-2 underline">Clients</h2>
-          <i className={`fa fa-caret-down ${isOpen.client && "open"}`}></i>
+    <>
+      <div
+        id="sub-navbar"
+        className="bg-white px-16 py-10 shadow-md flex justify-start"
+      >
+        <div className="dropdownbroker navbar-item">
+          <div className="dropdownbroker-header" onClick={toggleDropdownClient}>
+            <h2 className="mr-2 underline">Clients</h2>
+            <i className={`fa fa-caret-down ${isOpen.client && "open"}`}></i>
+          </div>
+
+          <div className={`dropdownbroker-body ${isOpen.client && "open"}`}>
+            <Link
+              to="/broker/dashboard/new-client"
+              className="dropdownbroker-item "
+            >
+              New Client{" "}
+            </Link>
+            <Link
+              to="/broker/dashboard/allclients"
+              className="dropdownbroker-item "
+            >
+              Manage Clients{" "}
+            </Link>
+          </div>
         </div>
 
-        <div className={`dropdownbroker-body ${isOpen.client && "open"}`}>
-          <Link
-            to="/broker/dashboard/new-client"
-            className="dropdownbroker-item "
-          >
-            New Client{" "}
-          </Link>
-          <Link
-            to="/broker/dashboard/allclients"
-            className="dropdownbroker-item "
-          >
-            Manage Clients{" "}
-          </Link>
+        <div className="navbar-item">
+          <h2 className="mr-2 underline">
+            <Link to="/broker/dashboard/upload">Upload users</Link>
+          </h2>
+        </div>
+        {breadCrumb !== undefined && " | "}
+        <div className="navbar-item ml-2">
+          {(function () {
+            if (breadCrumb !== undefined && breadCrumb.length === 1) {
+              return (
+                <Link to={breadCrumb[0].link} className="underline mx-2">
+                  {breadCrumb[0].name}
+                </Link>
+              );
+            }
+            
+            if (breadCrumb !== undefined && breadCrumb.length === 2) {
+
+              return breadCrumb.map((crumb, index) => {
+                return index === 0 ? (
+                  <>
+                    <Link key={index} to={crumb.link} className="underline mx-2">
+                      {crumb.name}
+                    </Link>
+                    {" / "}
+                  </>
+                ) : (
+                  <Link key={index} to={crumb.link} className="underline mx-2">
+                    {crumb.name}
+                  </Link>
+                );
+              });
+            }
+            
+            if(breadCrumb !== undefined && breadCrumb.length > 2){
+              return breadCrumb.map((crumb, index) => {
+                return (index + 1) % 2 === 0 ?(
+                  <>
+                  {" / "}
+                  <Link key={index} to={crumb.link} className="underline mx-2">
+                    {crumb.name}
+                  </Link>
+                  {" / "}
+                  </>
+                ):(
+                  <Link key={index} to={crumb.link} className="underline mx-2">
+                  {crumb.name}
+                  </Link>
+                )
+              });
+            }
+          })()}
         </div>
       </div>
-
-      <div className="navbar-item">
-        <h2 className="mr-2 underline">
-          <Link to="/broker/dashboard/upload">Upload users</Link>
-        </h2>
-      </div>
-    </div>
+    </>
   );
 }
