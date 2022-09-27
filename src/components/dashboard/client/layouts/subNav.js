4b77dd337cd0bdfa883aa296697c5776d8e6fbcb
romeo <romeomeshack34@gmail.com>
2022-09-27 15:49:59 +0100

    Modified

--- src/components/dashboard/client/layouts/subNav.js
+++ src/components/dashboard/client/layouts/subNav.js
@@ -7,7 +7,7 @@ export default function SubNav({ breadCrumbs }) {
       id="dashboard-dropdown"
       className="bg-white px-16 py-10 shadow-md flex justify-start"
     >
-      <div id="loan" className="dropdown-container">
+      {/* <div id="loan" className="dropdown-container">
         <div>
           Offers <i className="fa fa-caret-down mr-5" aria-hidden="true"></i>
         </div>
@@ -17,7 +17,50 @@ export default function SubNav({ breadCrumbs }) {
           <Link to="#">Drafts</Link>
         </div>
       </div>
-      {breadCrumbs !== undefined && " | "}
+      {breadCrumbs !== undefined && " | "} */}
+      <div className="navbar-item ml-2">
+          {(function () {
+            if (breadCrumbs !== undefined && breadCrumbs.length === 1) {
+              return (
+                <Link to={breadCrumbs[0].link} className="underline mx-2">
+                  {breadCrumbs[0].name}
+                </Link>
+              );
+            }
+
+            if (breadCrumbs !== undefined && breadCrumbs.length === 2) {
+              return breadCrumbs.map((crumb, index) => {
+                return index === 0 ? (
+                  <Link key={index} to={crumb.link} className="">
+                    <span className="underline">{crumb.name}</span>
+                    {"  "}
+                    <span className="">/</span>
+                  </Link>
+                ) : (
+                  <Link key={index} to={crumb.link} className="underline mx-2">
+                    {crumb.name}
+                  </Link>
+                );
+              });
+            }
+
+            if (breadCrumbs !== undefined && breadCrumbs.length > 2) {
+              return breadCrumbs.map((crumb, index) => {
+                return (index + 1) % 2 === 0 ? (
+                  <Link key={index} to={crumb.link} className="">
+                    <span>/</span>{" "}
+                    <span className="underline">{crumb.name}</span>{" "}
+                    <span>/</span>
+                  </Link>
+                ) : (
+                  <Link key={index} to={crumb.link} className="underline mx-2">
+                    {crumb.name}
+                  </Link>
+                );
+              });
+            }
+          })()}
+        </div>
     </div>
   );
 }
